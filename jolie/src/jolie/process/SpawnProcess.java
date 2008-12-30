55d104cb437c434e1de8cd0f3fce3ed6d491f192
Fabrizio Montesi <famontesi@gmail.com>
2008-12-30 10:20:18 +0000

    Support for the spawn statement.
    Former-commit-id: 9d0bc49f965f411aa95756a744b713047d9c35ec

---
+++ jolie/src/jolie/process/SpawnProcess.java
@@ -0,0 +1,89 @@
+/***************************************************************************
+ *   Copyright (C) by Fabrizio Montesi                                     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                         *
+ *   For details about the authors of this software, see the AUTHORS file. *
+ ***************************************************************************/
+
+package jolie.process;
+
+import jolie.runtime.FaultException;
+import jolie.runtime.Expression;
+import jolie.runtime.SpawnExecution;
+import jolie.runtime.VariablePath;
+
+public class SpawnProcess implements Process
+{
+	final private VariablePath indexPath;
+	final private VariablePath inPath; // may be null
+	final private Expression upperBound;
+	final private Process process;
+
+	public SpawnProcess(
+			VariablePath indexPath,
+			Expression upperBound,
+			VariablePath inPath,
+			Process process
+	)
+	{
+		this.indexPath = indexPath;
+		this.inPath = inPath;
+		this.upperBound = upperBound;
+		this.process = process;
+	}
+	
+	public Process clone( TransformationReason reason )
+	{
+		return new SpawnProcess(
+					(VariablePath) indexPath.cloneExpression( reason ),
+					upperBound.cloneExpression( reason ),
+					( inPath == null ) ? null : (VariablePath) inPath.cloneExpression( reason ),
+					process.clone( reason )
+				);
+	}
+	
+	public void run()
+		throws FaultException
+	{
+		new SpawnExecution( this ).run();
+	}
+
+	public Expression upperBound()
+	{
+		return upperBound;
+	}
+
+	public VariablePath indexPath()
+	{
+		return indexPath;
+	}
+
+	public VariablePath inPath()
+	{
+		return inPath;
+	}
+
+	public Process body()
+	{
+		return process;
+	}
+	
+	public boolean isKillable()
+	{
+		return process.isKillable();
+	}
+}
