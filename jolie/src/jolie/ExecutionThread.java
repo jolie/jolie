c55186d1423efa55564685964b2d319f9d8e5502
Fabrizio Montesi <famontesi@gmail.com>
2009-02-09 16:44:29 +0000

    CSet fix.

--- jolie/src/jolie/ExecutionThread.java
+++ jolie/src/jolie/ExecutionThread.java
@@ -363,31 +363,34 @@ abstract public class ExecutionThread extends JolieThread
 		Value correlationValue;
 		Value mValue = null;
 		Value cValue;
-		for( List< VariablePath > list : Interpreter.getInstance().correlationSet() ) {
+		for( List< VariablePath > list : interpreter().correlationSet() ) {
 			cValue = null;
-			correlationValue = list.get( 0 ).getValue();
-			for( VariablePath p : list ) {
-				if ( (path=recvPath.containedSubPath( p )) != null ) {
-					mValue = path.getValue( message.value() );
-					if (
-						correlationValue.isDefined() &&
-						!mValue.equals( correlationValue )
-					) {
-						return false;
-					} else if ( !correlationValue.isDefined() ) {
-						// Every correlated value must be equal
-						if ( cValue != null && !cValue.equals( mValue ) )
+			correlationValue = list.get( 0 ).getValueOrNull();
+			if ( correlationValue != null ) {
+				for( VariablePath p : list ) {
+					if ( (path=recvPath.containedSubPath( p )) != null ) {
+						mValue = path.getValueOrNull( message.value() );
+						if (
+							correlationValue.isDefined() &&
+							( mValue == null ||	!mValue.equals( correlationValue ) )
+						) {
 							return false;
-						else if ( cValue == null )
-							cValue = mValue;
+						} else if ( !correlationValue.isDefined() ) {
+							// Every correlated value must be equal
+							if ( cValue != null && mValue != null && !cValue.equals( mValue ) )
+								return false;
+							else if ( cValue == null )
+								cValue = mValue;
+						}
 					}
 				}
+
+				// TODO this must become a List!
+				// CSet values should be set only if the whole message is valid.
+				if ( cValue != null ) {
+					correlationValue.assignValue( cValue );
+				}
 			}
-			
-			// TODO this must become a List!
-			// CSet values should be set only if the whole message is valid.
-			if ( cValue != null )
-				correlationValue.assignValue( cValue );
 		}
 		
 		return true;
