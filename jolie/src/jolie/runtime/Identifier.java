6ff680cd631e095c216e2a6ff8ba870a20bae418
Fabrizio Montesi <famontesi@gmail.com>
2009-02-23 16:19:55 +0000

    Identifier: annotation for operation names overriding.
    Former-commit-id: 5d743ff937ada3e57996f69fcbe739c50ef3e1d8

---
+++ jolie/src/jolie/runtime/Identifier.java
@@ -0,0 +1,36 @@
+/***************************************************************************
+ *   Copyright (C) 2009 by Fabrizio Montesi                                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                         *
+ *   For details about the authors of this software, see the AUTHORS file. *
+ ***************************************************************************/
+
+package jolie.runtime;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Generic annotation for attaching a string identifier.
+ *
+ * @author Fabrizio Montesi
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Identifier
+{
+	String value();
+}
