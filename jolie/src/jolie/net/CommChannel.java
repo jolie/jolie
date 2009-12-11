9e4111b01ae0959525ed404de414e854a5b74f58
Fabrizio Montesi <famontesi@gmail.com>
2009-12-11 09:44:46 +0000

    Having a CommListener reference is not needed when handling an aggregated response.

--- jolie/src/jolie/net/CommChannel.java
+++ jolie/src/jolie/net/CommChannel.java
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2006-2008 by Fabrizio Montesi                           *
+ *   Copyright (C) 2006-2008 by Fabrizio Montesi <famontesi@gmail.com>     *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Library General Public License as       *
