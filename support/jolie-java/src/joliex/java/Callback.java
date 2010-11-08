b8585c0a3e4c40002f0e9db6482048ab62e56670
Fabrizio Montesi <famontesi@gmail.com>
2010-11-08 10:18:13 +0000

    Introducing jolie-java, a Java client library for invoking Jolie services.
    Former-commit-id: e573ba108d35349de0f3c9b33789e2e2b7daa89c

---
+++ support/jolie-java/src/joliex/java/Callback.java
@@ -0,0 +1,38 @@
+/***************************************************************************
+ *   Copyright (C) 2010 by Fabrizio Montesi <famontesi@gmail.com>          *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                         *
+ *   For details about the authors of this software, see the AUTHORS file. *
+ ***************************************************************************/
+
+
+package joliex.java;
+
+import java.io.IOException;
+import jolie.runtime.FaultException;
+import jolie.runtime.Value;
+
+/**
+ *
+ * @author Fabrizio Montesi
+ */
+public interface Callback
+{
+	public abstract void onSuccess( Value response );
+	public abstract void onFault( FaultException fault );
+	public abstract void onError( IOException exception );
+}
