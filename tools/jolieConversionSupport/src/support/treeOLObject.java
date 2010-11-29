36808989872ae00ef4c412d2e6276267654f3b19
Balint Maschio <bmaschio@italianasoftware.com>
2010-11-29 14:45:23 +0000

    Changed signature methods GeneralDocumentCreator
    Former-commit-id: 0f88825fda500807ec8f0b4c5ca05f0ac0f7ffe5

--- tools/jolieConversionSupport/src/support/treeOLObject.java
+++ tools/jolieConversionSupport/src/support/treeOLObject.java
@@ -5,10 +5,40 @@
 
 package support;
 
+import java.util.LinkedList;
+import java.util.List;
+import jolie.lang.parse.ast.OLSyntaxNode;
+
 /**
  *
  * @author balint
  */
 public class treeOLObject {
+private OLSyntaxNode olSyntaxNode;
+private List<treeOLObject> linkList;
+private String nameFile;
+private treeOLObject fatherObject;
+public treeOLObject(OLSyntaxNode olSyntaxNode, treeOLObject fatherObject){
+
+        this.olSyntaxNode=olSyntaxNode;
+        this.nameFile=olSyntaxNode.context().sourceName();
+        linkList= new LinkedList<treeOLObject>();
+        this.fatherObject=fatherObject;
+}
+public void SetLinkedObject(OLSyntaxNode olSyntaxNode){
+        treeOLObject supportObjet= new treeOLObject(olSyntaxNode,this);
+        linkList.add(supportObjet);
+   }
+public OLSyntaxNode GetLinkedObject(int i){
+return linkList.get(i).olSyntaxNode;
+}
+
+public int GetLinkedObjetSize(){
+
+ return linkList.size();
+}
+public treeOLObject GetFatherObject(){
+ return this.fatherObject;
+}
 
 }
