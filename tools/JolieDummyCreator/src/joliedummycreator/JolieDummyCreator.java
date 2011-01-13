a36dadf61d74782bb02717fe28663aed2a0c1364
Balint Maschio <bmaschio@italianasoftware.com>
2011-01-13 09:48:51 +0000

    Modify with the new
    Former-commit-id: 55ff2992188b36f7e3248c2d29e713ccba491d5b

--- tools/JolieDummyCreator/src/joliedummycreator/JolieDummyCreator.java
+++ tools/JolieDummyCreator/src/joliedummycreator/JolieDummyCreator.java
@@ -26,16 +26,20 @@ public class JolieDummyCreator {
      */
     public static void main(String[] args) throws IOException  {
        try{
-		CommandLineParser cmdParser =  new CommandLineParser( args, JolieDummyCreator.class.getClassLoader() );
-			args = cmdParser.arguments();
-
-
-			Program program = ParsingUtils.parseProgram(
-				cmdParser.programStream(),
+//		CommandLineParser cmdParser =  new CommandLineParser( args, JolieDummyCreator.class.getClassLoader() );
+//			args = cmdParser.arguments();
+//
+//
+//			Program program = ParsingUtils.parseProgram(
+//				cmdParser.programStream(),
+//				URI.create( "file:" + cmdParser.programFilepath() ),
+//				cmdParser.includePaths(), JolieDummyCreator.class.getClassLoader(), cmdParser.definedConstants()
+//			);
+		   JolieDummyCommandLineParser cmdParser= JolieDummyCommandLineParser.create( args, JolieDummyCommandLineParser.class.getClassLoader() );
+           Program program = ParsingUtils.parseProgram(
+			cmdParser.programStream(),
 				URI.create( "file:" + cmdParser.programFilepath() ),
-				cmdParser.includePaths(), JolieDummyCreator.class.getClassLoader(), cmdParser.definedConstants()
-			);
-
+				cmdParser.includePaths(), JolieDummyCreator.class.getClassLoader(), cmdParser.definedConstants());
 			ProgramInspector inspector=ParsingUtils.createInspector( program );
             JolieDummyDocumentCreator document= new JolieDummyDocumentCreator( inspector,cmdParser.programFilepath());
             document.createDocument();
---
+++ tools/JolieDummyCreator/src/joliedummycreator/formatExeption.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package joliedummycreator;
+
+/**
+ *
+ * @author balint
+ */
+public class formatExeption extends Exception {
+ public formatExeption()
+    {
+
+      super("Error");
+ }
+}
