31fcf88ddb2462518045e91f93a48920c8fb5a26
Fabrizio Montesi <famontesi@gmail.com>
2011-02-07 09:43:41 +0000

    Fix imports.
    Former-commit-id: ad4c4c5694921e817f80644bf38c390b4483a816

--- jolieJavaServices/src/joliex/db/DatabaseService.java
+++ jolieJavaServices/src/joliex/db/DatabaseService.java
@@ -29,7 +29,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 import jolie.runtime.CanUseJars;
 import jolie.runtime.FaultException;
