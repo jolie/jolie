a25a091c7f7dddb13a7712685ad6ce2638a21f48
Francesco Bullini <fbullini@italianasoftware.com>
2011-04-08 15:29:02 +0000

    added basic Scheduler JavaService quartz implementation
    Former-commit-id: b6cb0755456a667c7cf033936490b05a91bf9dcc

---
+++ jolieJavaServices/src/joliex/scheduler/JolieDefaultJob.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package joliex.scheduler;
+
+import jolie.net.CommMessage;
+import jolie.runtime.Value;
+import org.quartz.Job;
+import org.quartz.JobDataMap;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+/**
+ *
+ * @author Francesco
+ */
+public class JolieDefaultJob implements Job {
+
+	@Override
+	public void execute(JobExecutionContext context)
+			throws JobExecutionException {
+
+		JobDataMap data = context.getMergedJobDataMap();
+                String callbackOperation=data.getString("operation");
+		SchedulerService jsched=(SchedulerService) data.get("javaSchedulerService");
+                //System.out.println("callbackOperation = " + );
+                Value callbackValue = Value.create();
+                jsched.sendMessage( CommMessage.createRequest( callbackOperation, "/", callbackValue ) );
+	}
+
+
+}
