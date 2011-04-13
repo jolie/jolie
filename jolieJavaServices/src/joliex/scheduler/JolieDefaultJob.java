56df672a49854fd1e3f629c658bcffb5dc695b99
Fabrizio Montesi <famontesi@gmail.com>
2011-04-13 10:58:54 +0000

    - Update dependencies. - Code cleaning.
    Former-commit-id: e0eaf88e7f2ff7a9558f3d73c51d56c05897275d

--- jolieJavaServices/src/joliex/scheduler/JolieDefaultJob.java
+++ jolieJavaServices/src/joliex/scheduler/JolieDefaultJob.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package joliex.scheduler;
 
 import jolie.net.CommMessage;
@@ -16,19 +15,18 @@ import org.quartz.JobExecutionException;
  *
  * @author Francesco
  */
-public class JolieDefaultJob implements Job {
-
+public class JolieDefaultJob implements Job
+{
 	@Override
-	public void execute(JobExecutionContext context)
-			throws JobExecutionException {
+	public void execute( JobExecutionContext context )
+		throws JobExecutionException
+	{
 
 		JobDataMap data = context.getMergedJobDataMap();
-                String callbackOperation=data.getString("operation");
-		SchedulerService jsched=(SchedulerService) data.get("javaSchedulerService");
-                //System.out.println("callbackOperation = " + );
-                Value callbackValue = Value.create();
-                jsched.sendMessage( CommMessage.createRequest( callbackOperation, "/", callbackValue ) );
+		String callbackOperation = data.getString( "operation" );
+		SchedulerService jsched = (SchedulerService) data.get( "javaSchedulerService" );
+		//System.out.println("callbackOperation = " + );
+		Value callbackValue = Value.create();
+		jsched.sendMessage( CommMessage.createRequest( callbackOperation, "/", callbackValue ) );
 	}
-
-
 }
