e575446b4f43e0afe058e4975daa370b8da0c000
Fabrizio Montesi <famontesi@gmail.com>
2011-04-15 15:28:15 +0000

    Former-commit-id: 3a46239fad503aed4e482797f8b67ad82531a40f

--- playground/jHome/www/jhome.js
+++ playground/jHome/www/jhome.js
@@ -1,10 +1,4 @@
 $(document).ready(function(){
-	/*$.ajax({
-		url: 'getPageTitle',
-		dataType: 'xml',
-		success: function(response) { $("#title").html( $(response).find('getPageTitleResponse').text() ) }
-	});*/
-	
 	$.ajax({
 		url: 'getPageTitle',
 		dataType: 'json',
