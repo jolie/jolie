3dd661703e0c7b0de6d10972357a8fc84782dc89
Fabrizio Montesi <famontesi@gmail.com>
2011-05-17 13:50:44 +0000

    Former-commit-id: 063cc5f23df0b718a5de733fa97350e8ba2c77b0

---
+++ tools/JolieDummyCreator/src/joliex/dummycreator/FormatException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package joliex.dummycreator;
+
+/**
+ *
+ * @author Balint Maschio
+ */
+public class FormatException extends Exception
+{
+	public FormatException()
+	{
+		super( "Error" );
+	}
+}
