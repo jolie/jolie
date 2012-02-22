d88fc5e5842220e9db909602ed2adfe710e26738
Claudio Guidi <guidiclaudio@gmail.com>
2012-02-22 08:06:55 +0000

    substring operation fixed (StringUtils )

--- jolie/src/jolie/monitoring/events/OperationEndedEvent.java
+++ jolie/src/jolie/monitoring/events/OperationEndedEvent.java
@@ -14,13 +14,18 @@ import jolie.runtime.Value;
  * 17/02/2012
  */
 public class OperationEndedEvent extends MonitoringEvent {
+	public static final int SUCCESS = 0;
+	public static final int FAULT = 1;
+	public static final int ERROR = 2;
 
-	public OperationEndedEvent( String operation_name, String session_id ) {
+	public OperationEndedEvent( String operation_name, String session_id, int status, String details ) {
 
 		super( "OperationEnded", Value.create() );
 
 		data().getFirstChild( "operation_name" ).setValue( operation_name );
 		data().getFirstChild( "session_id" ).setValue( "session_id" );
+		data().getFirstChild( "status" ).setValue( status );
+		data().getFirstChild( "details" ).setValue( details );
 
 	}
 
