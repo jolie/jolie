aadfeeeb3bbc917672e77a8952a0d8c82857dbd3
Claudio Guidi <guidiclaudio@gmail.com>
2012-03-22 09:55:25 +0000

    adding NetworkService
    Former-commit-id: e6c6f7076c09fcf6f1e2f21a473c3d75c2f6d73d

---
+++ javaServices/coreJavaServices/src/joliex/util/NetworkService.java
@@ -0,0 +1,99 @@
+/***************************************************************************
+ *   Copyright (C) by Claudio Guidi	                                       *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                         *
+ *   For details about the authors of this software, see the AUTHORS file. *
+ ***************************************************************************/
+
+package joliex.util;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+import jolie.runtime.FaultException;
+import jolie.runtime.JavaService;
+import jolie.runtime.Value;
+import jolie.runtime.ValueVector;
+
+/**
+ *
+ * @author claudio
+ */
+public class NetworkService extends JavaService  {
+
+	public Value getNetworkInterfaceNames( Value request )
+		throws FaultException {
+
+		Value response = Value.create();
+		ValueVector interfaces = response.getChildren( "interface_name");
+
+		try {
+			Enumeration<NetworkInterface> list = NetworkInterface.getNetworkInterfaces();
+			int index = 0;
+			while (list.hasMoreElements()) {
+				NetworkInterface n = list.nextElement();
+				interfaces.get( index ).setValue( n.getName() );
+				index++;
+			}
+		} catch( SocketException e ) {
+			throw new FaultException( e );
+		}
+		return response;
+	}
+
+	public Value getIPs( Value request )
+		throws FaultException {
+
+		Value response = Value.create();
+
+		try {
+			Enumeration<NetworkInterface> list = NetworkInterface.getNetworkInterfaces();
+			boolean found = false;
+			while (list.hasMoreElements()) {
+				NetworkInterface n = list.nextElement();
+				
+				if ( n.getName().equals( request.getFirstChild("interface_name").strValue())) {
+					found = true;
+		
+					Enumeration<InetAddress> ad = n.getInetAddresses();
+					while( ad.hasMoreElements() ) {
+						InetAddress ia = ad.nextElement();
+						
+						if ( ia.getHostName().contains( "." ) ) {
+							// it is an IP4 address
+							response.getFirstChild( "ip4").setValue( ia.getHostName() );
+						} else {
+							// it is an IP6
+							response.getFirstChild( "ip6").setValue( ia.getHostName() );
+						}
+
+					}
+				}
+
+			}
+			if ( !found ) {
+				
+				throw new FaultException("InterfaceNotFound", new Exception() );
+			}
+		} catch( SocketException e ) {
+			throw new FaultException( e );
+		}
+		return response;
+
+	}
+}
