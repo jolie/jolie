b3cf23e04fd53f149ddb661135b4366fb407c843
Claudio Guidi <guidiclaudio@gmail.com>
2012-07-02 16:33:03 +0000

    bug fixes
    Former-commit-id: df982c0ddcd43b97fd8dce12745969d553093643

--- lib/jolie-xml/src/jolie/xml/xsd/XsdUtils.java
+++ lib/jolie-xml/src/jolie/xml/xsd/XsdUtils.java
@@ -42,10 +42,53 @@ public class XsdUtils
 		xsdToNativeTypeMap.put( "time", NativeType.STRING );
 		xsdToNativeTypeMap.put( "boolean", NativeType.BOOL );
 		xsdToNativeTypeMap.put( "int", NativeType.INT );
+		xsdToNativeTypeMap.put( "long", NativeType.LONG );
+		xsdToNativeTypeMap.put( "unsignedLong", NativeType.LONG );
 		xsdToNativeTypeMap.put( "integer", NativeType.INT );
+		xsdToNativeTypeMap.put( "nonNegativeInteger", NativeType.INT );
+		xsdToNativeTypeMap.put( "negativeInteger", NativeType.INT );
+		xsdToNativeTypeMap.put( "nonPositiveInteger", NativeType.INT );
+		xsdToNativeTypeMap.put( "positiveInteger", NativeType.INT );
+		xsdToNativeTypeMap.put( "unsignedShort", NativeType.INT );
+		xsdToNativeTypeMap.put( "unsignedInt", NativeType.INT );
+		xsdToNativeTypeMap.put( "short", NativeType.INT );
 		xsdToNativeTypeMap.put( "decimal", NativeType.DOUBLE );
 		xsdToNativeTypeMap.put( "anyType", NativeType.ANY );
+		xsdToNativeTypeMap.put( "anySimpleType", NativeType.ANY );
 		xsdToNativeTypeMap.put( "dateTime", NativeType.STRING );
+		xsdToNativeTypeMap.put( "time", NativeType.STRING );
+		xsdToNativeTypeMap.put( "duration", NativeType.STRING );
+		xsdToNativeTypeMap.put( "date", NativeType.STRING );
+		xsdToNativeTypeMap.put( "dateTime", NativeType.STRING );
+		xsdToNativeTypeMap.put( "gMonthDay", NativeType.STRING );
+		xsdToNativeTypeMap.put( "gYearDay", NativeType.STRING );
+		xsdToNativeTypeMap.put( "gDay", NativeType.STRING );
+		xsdToNativeTypeMap.put( "gMonth", NativeType.STRING );
+		xsdToNativeTypeMap.put( "gYearMonth", NativeType.STRING );
+		xsdToNativeTypeMap.put( "gYear", NativeType.STRING );
+		xsdToNativeTypeMap.put( "anyURI", NativeType.STRING );
+		xsdToNativeTypeMap.put( "normalizedString", NativeType.STRING );
+		xsdToNativeTypeMap.put( "base64Binary", NativeType.ANY );
+		xsdToNativeTypeMap.put( "base64", NativeType.ANY );
+		xsdToNativeTypeMap.put( "byte", NativeType.ANY );
+		xsdToNativeTypeMap.put( "unsignedByte", NativeType.ANY );
+		xsdToNativeTypeMap.put( "hexBinary", NativeType.ANY );
+		xsdToNativeTypeMap.put( "float", NativeType.DOUBLE );
+		xsdToNativeTypeMap.put( "double", NativeType.DOUBLE );
+		xsdToNativeTypeMap.put( "decimal", NativeType.DOUBLE );
+		xsdToNativeTypeMap.put( "ENTITIES", NativeType.STRING );
+		xsdToNativeTypeMap.put( "ENTITY", NativeType.STRING );
+		xsdToNativeTypeMap.put( "ID", NativeType.STRING );		// A string that represents the ID attribute in XML (only used with schema attributes)
+		xsdToNativeTypeMap.put( "IDREF", NativeType.STRING );	// A string that represents the IDREF attribute in XML (only used with schema attributes)
+		xsdToNativeTypeMap.put( "IDREFS", NativeType.STRING );
+		xsdToNativeTypeMap.put( "language", NativeType.STRING );  // A string that contains a valid language id
+		xsdToNativeTypeMap.put( "Name", NativeType.STRING );
+		xsdToNativeTypeMap.put( "NCName", NativeType.STRING );
+		xsdToNativeTypeMap.put( "NMTOKEN", NativeType.STRING );
+		xsdToNativeTypeMap.put( "NMTOKENS", NativeType.STRING );
+		xsdToNativeTypeMap.put( "normalizedString", NativeType.STRING );
+		xsdToNativeTypeMap.put( "QName", NativeType.STRING );
+		xsdToNativeTypeMap.put( "token", NativeType.STRING );
 	}
 
 	public static NativeType xsdToNativeType( String xsdTypeName )
