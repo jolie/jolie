999f7ed31ce77c055f1c0a4697cee3879d642754
Claudio Guidi <guidiclaudio@gmail.com>
2014-05-07 14:09:28 +0000

    bug fix: added 0 left padding to the output string. In case the md5 starts with 0, the toString method of BugInteger does not show the initial zeros.

--- javaServices/coreJavaServices/src/joliex/security/MessageDigestService.java
+++ javaServices/coreJavaServices/src/joliex/security/MessageDigestService.java
@@ -47,7 +47,18 @@ public class MessageDigestService extends JavaService
 		if ( (radix=request.getFirstChild( "radix" ).intValue()) < 2 ) {
 			radix = 16;
 		}
-
-		return new BigInteger( 1, md.digest() ).toString( radix );
+                
+                String response = new BigInteger( 1, md.digest() ).toString( radix );
+                if ( response.length() < 32 ) {
+                        int paddingLength = 32 - response.length();
+                        StringBuilder sb = new StringBuilder();
+                        for( int i = 0; i < paddingLength; i++ ) {
+                            sb.append("0");
+                        }
+                        sb.append( response );
+                        response = sb.toString();
+                }
+		return response;
+                
 	}
 }
