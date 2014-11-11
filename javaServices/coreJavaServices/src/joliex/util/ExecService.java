20683d1dc595e2a003a21296e64b94ab829a6e11
Fabrizio Montesi <famontesi@gmail.com>
2014-11-11 14:16:06 +0000

    Indentation fix.

--- javaServices/coreJavaServices/src/joliex/util/ExecService.java
+++ javaServices/coreJavaServices/src/joliex/util/ExecService.java
@@ -21,6 +21,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -29,89 +30,91 @@ import jolie.runtime.JavaService;
 import jolie.runtime.Value;
 import jolie.runtime.ValueVector;
 
-public class ExecService extends JavaService {
+public class ExecService extends JavaService
+{
+	public Value exec( Value request )
+		throws FaultException
+	{
+		List< String > command = new LinkedList< String >();
+		String[] str = request.strValue().split( " " );
+		command.addAll( Arrays.asList( str ) );
 
-    public Value exec(Value request)
-            throws FaultException {
-        List< String> command = new LinkedList< String>();
-        String[] str = request.strValue().split(" ");
-        for (int i = 0; i < str.length; i++) {
-            command.add(str[i]);
-        }
+		for( Value v : request.getChildren( "args" ) ) {
+			command.add( v.strValue() );
+		}
 
-        for (Value v : request.getChildren("args")) {
-            command.add(v.strValue());
-        }
+		ProcessBuilder builder = new ProcessBuilder( command );
+		if ( request.hasChildren( "workingDirectory" ) ) {
+			builder.directory( new File( request.getFirstChild( "workingDirectory" ).strValue() ) );
+		}
+		try {
 
-        ProcessBuilder builder = new ProcessBuilder(command);
-        if (request.hasChildren("workingDirectory")) {
-            builder.directory(new File(request.getFirstChild("workingDirectory").strValue()));
-        }
-        try {
+			Value response = Value.create();
+			boolean stdOutConsoleEnable = false;
+			Process p = builder.start();
+			StreamGobbler outputStreamGobbler = new StreamGobbler( p.getInputStream() );
+			if ( request.hasChildren( "stdOutConsoleEnable" ) ) {
+				if ( request.getFirstChild( "stdOutConsoleEnable" ).boolValue() ) {
+					outputStreamGobbler.start();
+					stdOutConsoleEnable = true;
+				}
+			}
+			ValueVector waitFor = request.children().get( "waitFor" );
+			if ( waitFor == null || waitFor.first().intValue() > 0 ) {
+				int exitCode = p.waitFor();
+				response.getNewChild( "exitCode" ).setValue( exitCode );
+				if ( !stdOutConsoleEnable ) {
+					int len = p.getInputStream().available();
+					if ( len > 0 ) {
+						char[] buffer = new char[ len ];
+						BufferedReader reader = new BufferedReader( new InputStreamReader( p.getInputStream() ) );
+						reader.read( buffer, 0, len );
+						response.setValue( new String( buffer ) );
+					}
+				}
+				if ( p.getErrorStream() != null ) {
+					int len = p.getErrorStream().available();
+					if ( len > 0 ) {
+						char[] buffer = new char[ len ];
+						BufferedReader reader = new BufferedReader( new InputStreamReader( p.getErrorStream() ) );
+						reader.read( buffer, 0, len );
+						response.getFirstChild( "stderr" ).setValue( new String( buffer ) );
+					}
+				}
 
-            Value response = Value.create();
-            boolean stdOutConsoleEnable = false;
-            Process p = builder.start();
-            StreamGobbler outputStreamGobbler = new StreamGobbler(p.getInputStream());
-            if (request.hasChildren("stdOutConsoleEnable")) {
-                if (request.getFirstChild("stdOutConsoleEnable").boolValue()) {
-                    outputStreamGobbler.start();
-                    stdOutConsoleEnable = true;
-                }
-            }
-            ValueVector waitFor = request.children().get("waitFor");
-            if (waitFor == null || waitFor.first().intValue() > 0) {
-                int exitCode = p.waitFor();
-                response.getNewChild("exitCode").setValue(exitCode);
-                if (!stdOutConsoleEnable) {
-                    int len = p.getInputStream().available();
-                    if (len > 0) {
-                        char[] buffer = new char[len];
-                        BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-                        reader.read(buffer, 0, len);
-                        response.setValue(new String(buffer));
-                    }
-                }
-                if (p.getErrorStream() != null) {
-                    int len = p.getErrorStream().available();
-                    if (len > 0) {
-                        char[] buffer = new char[len];
-                        BufferedReader reader = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-                        reader.read(buffer, 0, len);
-                        response.getFirstChild("stderr").setValue(new String(buffer));
-                    }
-                }
+				p.getInputStream().close();
+				p.getErrorStream().close();
+				p.getOutputStream().close();
+			}
+			return response;
+		} catch( Exception e ) {
+			throw new FaultException( e );
+		}
+	}
 
-                p.getInputStream().close();
-                p.getErrorStream().close();
-                p.getOutputStream().close();
-            }
-            return response;
-        } catch (Exception e) {
-            throw new FaultException(e);
-        }
-    }
+	private class StreamGobbler extends Thread
+	{
 
-    private class StreamGobbler extends Thread {
+		InputStream is;
 
-        InputStream is;
+		private StreamGobbler( InputStream is )
+		{
+			this.is = is;
+		}
 
-        private StreamGobbler(InputStream is) {
-            this.is = is;
-        }
-
-        @Override
-        public void run() {
-            try {
-                InputStreamReader isr = new InputStreamReader(is);
-                BufferedReader br = new BufferedReader(isr);
-                String line = null;
-                while ((line = br.readLine()) != null) {
-                    System.out.println(line);
-                }
-            } catch (IOException ioe) {
-                ioe.printStackTrace();
-            }
-        }
-    }
+		@Override
+		public void run()
+		{
+			try {
+				InputStreamReader isr = new InputStreamReader( is );
+				BufferedReader br = new BufferedReader( isr );
+				String line = null;
+				while( (line = br.readLine()) != null ) {
+					System.out.println( line );
+				}
+			} catch( IOException ioe ) {
+				ioe.printStackTrace();
+			}
+		}
+	}
 }
