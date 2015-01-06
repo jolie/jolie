598fc62d2af9457c241d2a793ad781a831c582e5
Fabrizio Montesi <famontesi@gmail.com>
2015-01-06 09:03:15 +0000

    Code indentation.

--- javaServices/coreJavaServices/src/joliex/io/FileService.java
+++ javaServices/coreJavaServices/src/joliex/io/FileService.java
@@ -31,7 +31,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-
 import java.io.Writer;
 import java.net.URL;
 import java.net.URLConnection;
