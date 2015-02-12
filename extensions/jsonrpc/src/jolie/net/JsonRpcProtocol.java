2a84aa8800fe2d57eb2800753ca3e19a0636b523
Matthias Dieter Wallnöfer <mdw@samba.org>
2015-02-12 15:24:52 +0000

    http extensions - the HTTP headers should be generated with the correct encoding. All 127 ASCII chars are contained in UTF-8, so this should work everywhere, also on Windows.

--- extensions/jsonrpc/src/jolie/net/JsonRpcProtocol.java
+++ extensions/jsonrpc/src/jolie/net/JsonRpcProtocol.java
@@ -94,7 +94,7 @@ public class JsonRpcProtocol extends ConcurrentCommProtocol
 		if (!message.isFault() && message.hasGenericId() && inInputPort) {
 			// JSON-RPC notification mechanism (method call with dropped result)
 			// we just send HTTP status code 204
-			Writer writer = new OutputStreamWriter(ostream);
+			Writer writer = new OutputStreamWriter( ostream, "utf-8" );
 			writer.write("HTTP/1.1 204 No Content" + HttpUtils.CRLF);
 			writer.write("Server: Jolie" + HttpUtils.CRLF + HttpUtils.CRLF);
 			writer.flush();
@@ -171,7 +171,7 @@ public class JsonRpcProtocol extends ConcurrentCommProtocol
 			interpreter.logInfo("[JSON-RPC debug] Sending:\n" + httpMessage.toString() + content.toString( "utf-8" ));
 		}
 
-		Writer writer = new OutputStreamWriter(ostream);
+		Writer writer = new OutputStreamWriter( ostream, "utf-8" );
 		writer.write( httpMessage.toString() );
 		writer.flush();
 		ostream.write( content.getBytes() );
