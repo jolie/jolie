3f1b6efe7dcf00208cac215b7b053a4a73323af9
Matthias Dieter Wallnöfer <mdw@samba.org>
2015-02-26 09:40:43 +0000

    XML-RPC protocol improvements

--- extensions/xmlrpc/src/jolie/net/XmlRpcProtocol.java
+++ extensions/xmlrpc/src/jolie/net/XmlRpcProtocol.java
@@ -1,5 +1,6 @@
 /***************************************************************************
  *   Copyright (C) 2009 by Claudio Guidi                                   *
+ *   Copyright (C) 2015 by Matthias Dieter Wallnöfer                       *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Library General Public License as       *
@@ -67,8 +68,6 @@ import org.w3c.dom.NodeList;
  * 
  */
 
-// TO DO: faults
-
 /* NOTE about this module:
  * The XML-RPC specification reference considered is http://www.xmlrpc.com/spec
  * I made different assumptions for input and output messages.
@@ -76,11 +75,10 @@ import org.w3c.dom.NodeList;
  * aliases for operation names are allowed because Jolie does not support operation names with dots (es. mickey.mouse)
  * aliases are expressed as protocol parameters as aliases.opname = "aliasName".
  * In output the request message must contain a subelement param (which could be an array).
- * Each element of teh subelement param is translated into a tag <param>.
+ * Each element of the subelement param is translated into a tag <param>.
  * in order to express an array within an element of param, you need to use the keyword array:
  * request.param.array[0]=....; request.param.array[1]=...
  * will be translated into <param><value><array><data><value>...</value><value>...</value>..</data></array></value></param>
- * Since boolean are not managed by Jolie, it is necessary to represent it with subelement boolean
  *
  * Output Faults:
  * At the present Jolie always generates zero code faults.
@@ -100,6 +98,24 @@ public class XmlRpcProtocol extends SequentialCommProtocol
 	private final boolean inInputPort;
 	private boolean received = false;
 	private String encoding;
+
+	/**
+	 * In XML-RPC each request or response parameter needs to be contained
+	 * in a variable with this name, which is usually an array. Otherwise the
+	 * parameter gets ignored.
+	 */
+	private static final String PARAMS_KEY = "param";
+
+	/**
+	 * XML-RPC arrays are mapped to Jolie arrays called "ARRAY_KEY" as
+	 * children of the respective values (= placeholders). Hence these values
+	 * should not carry any root value and also not contain any other
+	 * children.
+	 * Eg. <param><array><data><value><int>3</int></value></data></param>
+	 * in XML-RPC becomes param[0].array[0] = 3 in Jolie and param[0] should
+	 * not contain anything beside the array.
+	 */
+	private static final String ARRAY_KEY = "array";
 
 	public String name()
 	{
@@ -147,27 +163,34 @@ public class XmlRpcProtocol extends SequentialCommProtocol
 		}
 		Node contentNode = contents.item( 0 );
 		if ( contentNode.getNodeType() != Node.ELEMENT_NODE ) {
-			throw new IOException( "a value node may contain only an sub-element" );
+			throw new IOException( "a value node may contain only one sub-element" );
 		}
 
 		Element content = (Element)contentNode;
 		String name = content.getNodeName();
 		if ( name.equals( "array" ) ) {
 			Value currentValue;
-			ValueVector vec = value.getChildren( "array" );
-			NodeList dataChildren = getFirstElement( content, "data" ).getElementsByTagName( "value" );
+			ValueVector vec = value.getChildren( ARRAY_KEY );
+			Element data = getFirstElement( content, "data" );
+			NodeList dataChildren = data.getElementsByTagName( "value" );
 			for ( int i = 0; i < dataChildren.getLength(); i++ ) {
+				Element member = (Element)dataChildren.item( i );
+				if ( !member.getParentNode().equals( data ) ) {
+					continue; // not a direct child
+				}
 				currentValue = Value.create();
-				navigateValue( currentValue, (Element)dataChildren.item( i ) );
+				navigateValue( currentValue, member );
 				vec.add( currentValue );
 			}
 		} else if ( name.equals( "struct" ) ) {
 			NodeList members = content.getElementsByTagName( "member" );
-			Element member;
 			for ( int i = 0; i < members.getLength(); i++ ) {
-				member = (Element)members.item( i );
+				Element member = (Element)members.item( i );
+				if ( !member.getParentNode().equals( content ) ) {
+					continue; // not a direct child
+				}
 				Element valueNode = getFirstElement( member, "value" );
-				navigateValue( value.getNewChild( getFirstElement( member, "name" ).getNodeValue() ), valueNode );
+				navigateValue( value.getNewChild( getFirstElement( member, "name" ).getTextContent() ), valueNode );
 			}
 		} else if ( name.equals( "string" ) ) {
 			value.setValue( content.getTextContent() );
@@ -177,25 +200,36 @@ public class XmlRpcProtocol extends SequentialCommProtocol
 			} catch( NumberFormatException e ) {
 				throw new IOException( e );
 			}
+		} else if ( name.equals( "double" ) ) {
+			try {
+				value.setValue( Double.parseDouble( content.getTextContent() ) );
+			} catch( NumberFormatException e ) {
+				throw new IOException( e );
+			}
 		} else if ( name.equals( "boolean" ) ) {
 			try {
 				value.setValue( Integer.parseInt( content.getTextContent() ) != 0);
 			} catch( NumberFormatException e ) {
 				throw new IOException( e );
 			}
+		} else if ( name.equals( "base64" ) ) {
+			value.setValue( new ByteArray( new sun.misc.BASE64Decoder().decodeBuffer( content.getTextContent() ) ) );
+		} else {
+			// parse everything else as string (including <dateTime.iso8601>)
+			value.setValue( content.getTextContent() );
 		}
 	}
 
 	private void documentToValue( Value value, Document document )
 		throws IOException
 	{
-		ValueVector paramsValueVector = value.getChildren( "param" );
+		ValueVector paramsValueVector = value.getChildren( PARAMS_KEY );
 
 		NodeList params = getFirstElement( document.getDocumentElement(), "params" ).getElementsByTagName( "param" );
-		Value paramValue;
 		for( int i = 0; i < params.getLength(); i++ ) {
-			paramValue = Value.create();
-			navigateValue( paramValue, getFirstElement( ((Element)params.item( i )), "value" ) );
+			Element param = (Element)params.item( i );
+			Value paramValue = Value.create();
+			navigateValue( paramValue, getFirstElement( param, "value" ) );
 			paramsValueVector.add( paramValue );
 		}
 	}
@@ -234,21 +268,21 @@ public class XmlRpcProtocol extends SequentialCommProtocol
 			v.appendChild( b );
 			node.appendChild( v );
 			
-		} else if ( value.hasChildren( "array" ) ) {
+		} else if ( value.hasChildren( ARRAY_KEY ) ) {
 			// array creation
 
 			Element a = doc.createElement( "array" );
 			Element d = doc.createElement( "data" );
-			for ( int i = 0; i < value.getChildren( "array" ).size(); i++ ) {
-				valueToDocument( value.getChildren( "array" ).get( i ), d, doc );
+			for ( int i = 0; i < value.getChildren( ARRAY_KEY ).size(); i++ ) {
+				valueToDocument( value.getChildren( ARRAY_KEY ).get( i ), d, doc );
 			}
 			a.appendChild( d );
 			v.appendChild( a );
 			node.appendChild( v );
-		} else {
+		} else if ( value.hasChildren() ) {
+			Element st = doc.createElement( "struct" );
 			for ( Entry<String, ValueVector> entry : value.children().entrySet() ) {
 				if ( !entry.getKey().startsWith( "@" ) ) {
-					Element st = doc.createElement( "struct" );
 					Element m = doc.createElement( "member" );
 					Element n = doc.createElement( "name" );
 					n.appendChild( doc.createTextNode( entry.getKey() ) );
@@ -257,12 +291,11 @@ public class XmlRpcProtocol extends SequentialCommProtocol
 						valueToDocument( val, m, doc );
 					}
 					st.appendChild( m );
-					v.appendChild( st );
 				}
 			}
+			v.appendChild( st );
+			node.appendChild( v );
 		}
-
-
 	}
 
 	private void send_internal( OutputStream ostream, CommMessage message, InputStream istream )
@@ -303,75 +336,34 @@ public class XmlRpcProtocol extends SequentialCommProtocol
 			Element m1 = doc.createElement( "member" );
 			Element n1 = doc.createElement( "name" );
 			Element v1 = doc.createElement( "value" );
-			Element i1 = doc.createElement( "i4" );
+			Element i1 = doc.createElement( "int" );
 			Element m2 = doc.createElement( "member" );
 			Element n2 = doc.createElement( "name" );
 			Element v2 = doc.createElement( "value" );
 			Element i2 = doc.createElement( "string" );
+			n1.setTextContent( "faultCode" );
 			i1.setTextContent( "0" ); // Jolie generates always zero code faults
-			i2.setTextContent( f.faultName() ); // fault name is insertied into faultString tag of XMLRPC message
+			n2.setTextContent( "faultString" );
+			// the XML-RPC specification allows us only to set this value
+			i2.setTextContent( f.value().strValue() );
 			v1.appendChild( i1 );
 			v2.appendChild( i2 );
-			n1.setTextContent( "faultCode" );
-			m1.appendChild( v1 );
 			m1.appendChild( n1 );
-			m2.appendChild( v2 );
+			m1.appendChild( v1 );
 			m2.appendChild( n2 );
+			m2.appendChild( v2 );
 			s.appendChild( m1 );
 			s.appendChild( m2 );
 			v.appendChild( s );
 			fault.appendChild( v );
 			root.appendChild( fault );
 
-		} else if ( message.value().hasChildren( "param" ) ) {
+		} else if ( message.value().hasChildren( PARAMS_KEY ) ) {
 			// params exist
 			Element params = doc.createElement( "params" );
-			for ( int i = 0; i < message.value().getChildren( "param" ).size(); i++ ) {
+			for ( int i = 0; i < message.value().getChildren( PARAMS_KEY ).size(); i++ ) {
 				Element p = doc.createElement( "param" );
-				Element v = doc.createElement( "value" );
-				if ( message.value().getChildren( "param" ).get( i ).isInt() ) {
-					Element in = doc.createElement( "int" );
-					in.appendChild( doc.createTextNode( message.value().getChildren( "param" ).get( i ).strValue() ) );
-					v.appendChild( in );
-				} else if ( message.value().getChildren( "param" ).get( i ).isString() ) {
-					Element s = doc.createElement( "string" );
-					s.appendChild( doc.createTextNode( message.value().getChildren( "param" ).get( i ).strValue() ) );
-					v.appendChild( s );
-				} else if ( message.value().getChildren( "param" ).get( i ).isDouble() ) {
-					Element d = doc.createElement( "double" );
-					d.appendChild( doc.createTextNode( message.value().getChildren( "param" ).get( i ).strValue() ) );
-					v.appendChild( d );
-				} else if ( message.value().getChildren( "param" ).get( i ).isBool() ) {
-					Element b = doc.createElement( "boolean" );
-					b.appendChild( doc.createTextNode( message.value().getChildren( "param" ).get( i ).boolValue() ? "1" : "0" ) );
-					v.appendChild( b );
-				} else if ( message.value().getChildren( "param" ).get( i ).hasChildren( "array" ) ) {
-					// array creation
-					Element a = doc.createElement( "array" );
-					Element d = doc.createElement( "data" );
-					for ( int x = 0; x < message.value().getChildren( "param" ).get( i ).getChildren( "array" ).size(); x++ ) {
-						valueToDocument( message.value().getChildren( "param" ).get( i ).getChildren( "array" ).get( x ), d, doc );
-					}
-					a.appendChild( d );
-					v.appendChild( a );
-				} else if ( message.value().getChildren( "param" ).get( i ).hasChildren() ) {
-					for ( Entry<String, ValueVector> entry : message.value().getChildren( "param" ).get( i ).children().entrySet() ) {
-						if ( !entry.getKey().startsWith( "@" ) ) {
-							Element st = doc.createElement( "struct" );
-							Element m = doc.createElement( "member" );
-							Element n = doc.createElement( "name" );
-							n.appendChild( doc.createTextNode( entry.getKey() ) );
-							m.appendChild( n );
-							for ( Value val : entry.getValue() ) {
-								valueToDocument( val, m, doc );
-							}
-							st.appendChild( m );
-							v.appendChild( st );
-						}
-
-					}
-				}
-				p.appendChild( v );
+				valueToDocument( message.value().getChildren( PARAMS_KEY ).get( i ), p, doc );
 				params.appendChild( p );
 			}
 			root.appendChild( params );
