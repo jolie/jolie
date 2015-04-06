443733cc72a2283ce692e5db572a5c72bd55f377
Matthias Dieter Wallnöfer <mdw@samba.org>
2015-04-06 09:57:03 +0200

    http extension - some more compressable types
    Former-commit-id: a0bb211a03312d7475893f9271f494e235ee558b

--- extensions/http/src/jolie/net/HttpProtocol.java
+++ extensions/http/src/jolie/net/HttpProtocol.java
@@ -776,7 +776,7 @@ public class HttpProtocol extends CommProtocol implements HttpUtils.HttpProtocol
 			boolean compression = encoding != null && checkBooleanParameter( Parameters.COMPRESSION, true );
 			String compressionTypes = getStringParameter(
 				Parameters.COMPRESSION_TYPES,
-				"text/html text/css text/plain text/xml text/x-js application/json application/javascript"
+				"text/html text/css text/plain text/xml text/x-js application/json application/javascript application/x-www-form-urlencoded text/x-gwt-rpc"
 			).toLowerCase();
 			if ( compression && !compressionTypes.equals( "*" ) && !compressionTypes.contains( encodedContent.contentType ) ) {
 				compression = false;
