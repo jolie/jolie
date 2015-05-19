f6ab124dccdb7f910bcdcfce4a0288423e62604b
mawo <mw@martinwolf.eu>
2015-05-19 20:20:54 +0200

    Following the code guidelines this time ;-)
    Former-commit-id: c44d1bcf55c7af58d88ab19f0bfab20aa4ac9bc3

--- jolie/src/jolie/runtime/embedding/EmbeddedServiceLoader.java
+++ jolie/src/jolie/runtime/embedding/EmbeddedServiceLoader.java
@@ -39,145 +39,144 @@ public abstract class EmbeddedServiceLoader
 	}
 
 	private static EmbeddedServiceLoader createLoader(
-				Interpreter interpreter,
-                EmbeddedServiceConfiguration configuration,				
-                Expression channelDest
-			)
+		Interpreter interpreter,
+		EmbeddedServiceConfiguration configuration,
+		Expression channelDest
+	)
 		throws EmbeddedServiceLoaderCreationException
 	{
 		EmbeddedServiceLoader ret = null;
 		try {
-            if ( configuration.isInternal()) {
-                InternalEmbeddedServiceConfiguration internalConfiguration = (InternalEmbeddedServiceConfiguration) configuration;
-                ret = new InternalJolieServiceLoader(channelDest, interpreter, internalConfiguration.getServiceName(), internalConfiguration.getProgram());
-            }
-            else {
-                ExternalEmbeddedServiceConfiguration externalConfiguration = (ExternalEmbeddedServiceConfiguration) configuration;
-                switch(configuration.getType()) {
-                    case JAVA:
-                        ret = new JavaServiceLoader(channelDest, externalConfiguration.getServicePath(), interpreter);
-                        break;
-                    case JOLIE:
-                        ret = new JolieServiceLoader(channelDest, interpreter, externalConfiguration.getServicePath());
-                        break;
-                    case JAVASCRIPT:
-                        ret = new JavaScriptServiceLoader(channelDest, externalConfiguration.getServicePath());
-                        break;
-                    default:
-                        throw new EmbeddedServiceLoaderCreationException( "Invalid embedded service type specified" );
-                } 
-            }
+			if ( configuration.isInternal() ) {
+				InternalEmbeddedServiceConfiguration internalConfiguration = (InternalEmbeddedServiceConfiguration) configuration;
+				ret = new InternalJolieServiceLoader( channelDest, interpreter, internalConfiguration.serviceName(), internalConfiguration.program() );
+			} else {
+				ExternalEmbeddedServiceConfiguration externalConfiguration = (ExternalEmbeddedServiceConfiguration) configuration;
+				switch( configuration.type() ) {
+					case JAVA:
+						ret = new JavaServiceLoader( channelDest, externalConfiguration.servicePath(), interpreter );
+						break;
+					case JOLIE:
+						ret = new JolieServiceLoader( channelDest, interpreter, externalConfiguration.servicePath() );
+						break;
+					case JAVASCRIPT:
+						ret = new JavaScriptServiceLoader( channelDest, externalConfiguration.servicePath() );
+						break;
+					default:
+						throw new EmbeddedServiceLoaderCreationException( "Invalid embedded service type specified" );
+				}
+			}
 		} catch( Exception e ) {
 			throw new EmbeddedServiceLoaderCreationException( e );
 		}
 
 		return ret;
 	}
-	
+
 	public static EmbeddedServiceLoader create(
-				Interpreter interpreter,
-				EmbeddedServiceConfiguration configuration,
-				Value channelValue
-			)
+		Interpreter interpreter,
+		EmbeddedServiceConfiguration configuration,
+		Value channelValue
+	)
 		throws EmbeddedServiceLoaderCreationException
 	{
 		return createLoader( interpreter, configuration, channelValue );
 	}
 
 	public static EmbeddedServiceLoader create(
-				Interpreter interpreter,
-				EmbeddedServiceConfiguration configuration,
-				VariablePath channelPath
-			)
+		Interpreter interpreter,
+		EmbeddedServiceConfiguration configuration,
+		VariablePath channelPath
+	)
 		throws EmbeddedServiceLoaderCreationException
 	{
 		return createLoader( interpreter, configuration, channelPath );
-    }
+	}
 
 	protected void setChannel( CommChannel channel )
 	{
 		if ( channelDest != null ) {
 			if ( channelDest instanceof VariablePath ) {
-				((VariablePath)channelDest).getValue().setValue( channel );
+				((VariablePath) channelDest).getValue().setValue( channel );
 			} else if ( channelDest instanceof Value ) {
-				((Value)channelDest).setValue( channel );
+				((Value) channelDest).setValue( channel );
 			}
 		}
 	}
-	
+
 	abstract public void load()
 		throws EmbeddedServiceLoadingException;
 
-    public static abstract class EmbeddedServiceConfiguration
-    {
-        private final Constants.EmbeddedServiceType type;
-        
-        public EmbeddedServiceConfiguration(Constants.EmbeddedServiceType type)
-        {
-            this.type = type;
-        }
-        
-        public Constants.EmbeddedServiceType getType()
-        {
-            return this.type;
-        }
-        
-        public boolean isInternal()
-        {
-            return this.type.equals(Constants.EmbeddedServiceType.INTERNAL);
-        }
-    }
-    
-    public static class InternalEmbeddedServiceConfiguration extends EmbeddedServiceConfiguration
-    {
-        private final String serviceName;
-        private final Program program;
+	public static abstract class EmbeddedServiceConfiguration
+	{
+		private final Constants.EmbeddedServiceType type;
 
-        /**
-         *
-         * @param serviceName Name of the internal service.
-         * @param program the program containing the service
-         */
-        public InternalEmbeddedServiceConfiguration( String serviceName, Program program)
-        {
-            super(Constants.EmbeddedServiceType.INTERNAL);
-            
-            this.serviceName = serviceName;
-            this.program = program;
-        }
-        
-        public String getServiceName()
-        {
-            return serviceName;
-        }
+		public EmbeddedServiceConfiguration( Constants.EmbeddedServiceType type )
+		{
+			this.type = type;
+		}
 
-        public Program getProgram()
-        {
-            return program;
-        }
-    }
-    
-    public static class ExternalEmbeddedServiceConfiguration extends EmbeddedServiceConfiguration
-    {
-        private final String servicePath;
+		public Constants.EmbeddedServiceType type()
+		{
+			return this.type;
+		}
 
-        /**
-         *
-         * @param type Type of embedded service, cannot be INTERNAL
-         * @param servicePath path of service
-         */
-        public ExternalEmbeddedServiceConfiguration( Constants.EmbeddedServiceType type, String servicePath )
-        {
-            super(type);
-            this.servicePath = servicePath;
-            
-            assert type != Constants.EmbeddedServiceType.INTERNAL;
-        }
-                
-        public String getServicePath()
-        {
-            return servicePath;
-        }
+		public boolean isInternal()
+		{
+			return this.type.equals( Constants.EmbeddedServiceType.INTERNAL );
+		}
+	}
 
-    }
+	public static class InternalEmbeddedServiceConfiguration extends EmbeddedServiceConfiguration
+	{
+		private final String serviceName;
+		private final Program program;
+
+		/**
+		 *
+		 * @param serviceName Name of the internal service.
+		 * @param program the program containing the service
+		 */
+		public InternalEmbeddedServiceConfiguration( String serviceName, Program program )
+		{
+			super( Constants.EmbeddedServiceType.INTERNAL );
+
+			this.serviceName = serviceName;
+			this.program = program;
+		}
+
+		public String serviceName()
+		{
+			return serviceName;
+		}
+
+		public Program program()
+		{
+			return program;
+		}
+	}
+
+	public static class ExternalEmbeddedServiceConfiguration extends EmbeddedServiceConfiguration
+	{
+		private final String servicePath;
+
+		/**
+		 *
+		 * @param type Type of embedded service, cannot be INTERNAL
+		 * @param servicePath path of service
+		 */
+		public ExternalEmbeddedServiceConfiguration( Constants.EmbeddedServiceType type, String servicePath )
+		{
+			super( type );
+			this.servicePath = servicePath;
+
+			assert type != Constants.EmbeddedServiceType.INTERNAL;
+		}
+
+		public String servicePath()
+		{
+			return servicePath;
+		}
+
+	}
 }
