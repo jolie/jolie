9027f794922d41b04ab385e9a07320476da77149
fmontesi <famontesi@gmail.com>
2015-05-20 09:21:23 +0200

    Update javascript test

--- test/primitives/private/TwiceService.js
+++ test/primitives/private/TwiceService.js
@@ -1,5 +1,6 @@
 function twice( request )
 {
-	var number = request.getFirstChild("number").intValue();
+	// var number = request.getFirstChild("number").intValue();
+	var number = request.number;
 	return number + number;
 }
