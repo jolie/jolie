7967930f7cae8907f131ddd21ff151d4a52607f1
fmontesi <famontesi@gmail.com>
2015-07-16 09:15:15 +0200

    Code cleanup.

--- jolie/src/jolie/process/ExitProcess.java
+++ jolie/src/jolie/process/ExitProcess.java
@@ -1,23 +1,21 @@
-/***************************************************************************
- *   Copyright (C) by Fabrizio Montesi                                     *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU Library General Public License as       *
- *   published by the Free Software Foundation; either version 2 of the    *
- *   License, or (at your option) any later version.                       *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU Library General Public     *
- *   License along with this program; if not, write to the                 *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
- *                                                                         *
- *   For details about the authors of this software, see the AUTHORS file. *
- ***************************************************************************/
+/*
+ * Copyright (C) 2007-2015 Fabrizio Montesi <famontesi@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
 
 package jolie.process;
 
@@ -30,7 +28,7 @@ public class ExitProcess implements Process
 	
 	private static class LazyHolder {
 		private LazyHolder() {}
-		static final ExitProcess instance = new ExitProcess();
+		private static final ExitProcess instance = new ExitProcess();
 	}
 	
 	static public ExitProcess getInstance()
@@ -38,21 +36,20 @@ public class ExitProcess implements Process
 		return ExitProcess.LazyHolder.instance;
 	}
 	
+	@Override
 	public Process clone( TransformationReason reason )
 	{
 		return getInstance();
 	}
 	
+	@Override
 	public void run()
 	{
 		final Interpreter interpreter = Interpreter.getInstance();
-		interpreter.execute( new Runnable() {
-			public void run() {
-				interpreter.exit();
-			}
-		});
+		interpreter.execute( () -> interpreter.exit() );
 	}
 	
+	@Override
 	public boolean isKillable()
 	{
 		return true;
--- jolie/src/jolie/process/IfProcess.java
+++ jolie/src/jolie/process/IfProcess.java
@@ -1,23 +1,21 @@
-/***************************************************************************
- *   Copyright (C) by Fabrizio Montesi                                     *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU Library General Public License as       *
- *   published by the Free Software Foundation; either version 2 of the    *
- *   License, or (at your option) any later version.                       *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU Library General Public     *
- *   License along with this program; if not, write to the                 *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
- *                                                                         *
- *   For details about the authors of this software, see the AUTHORS file. *
- ***************************************************************************/
+/*
+ * Copyright (C) 2006-2015 Fabrizio Montesi <famontesi@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
 
 package jolie.process;
 
@@ -26,8 +24,7 @@ import jolie.runtime.ExitingException;
 import jolie.runtime.FaultException;
 import jolie.runtime.expression.Expression;
 
-
-public class IfProcess implements Process
+public final class IfProcess implements Process
 {
 	public static class CPPair
 	{
@@ -60,14 +57,15 @@ public class IfProcess implements Process
 		this.elseProcess = elseProcess;
 	}
 	
+	@Override
 	public Process clone( TransformationReason reason )
 	{
 		CPPair[] pairsCopy = new CPPair[ pairs.length ];
 		for( int i = 0; i < pairs.length; i++ ) {
 			pairsCopy[ i ] = new CPPair(
-					pairs[ i ].condition.cloneExpression( reason ),
-					pairs[ i ].process.clone( reason )
-				);
+				pairs[ i ].condition.cloneExpression( reason ),
+				pairs[ i ].process.clone( reason )
+			);
 		}
 		return new IfProcess(
 			pairsCopy,
@@ -75,6 +73,7 @@ public class IfProcess implements Process
 		);
 	}
 	
+	@Override
 	public void run()
 		throws FaultException, ExitingException
 	{
@@ -84,10 +83,9 @@ public class IfProcess implements Process
 
 		boolean keepRun = true;
 		int i = 0;
-		CPPair pair;
-		
+	
 		while( keepRun && i < pairs.length ) {
-			pair = pairs[ i ];
+			final CPPair pair = pairs[ i ];
 			if ( pair.condition().evaluate().boolValue() ) {
 				keepRun = false;
 				pair.process().run();
@@ -101,6 +99,7 @@ public class IfProcess implements Process
 		}
 	}
 	
+	@Override
 	public boolean isKillable()
 	{
 		return true;
