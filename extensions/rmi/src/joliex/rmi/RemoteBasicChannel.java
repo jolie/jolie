066d12efa8cafc615bc0fee52a11a30e6f86d2f2
Fabrizio Montesi <famontesi@gmail.com>
2015-08-11 10:01:32 +0200

    Declare throwable IOException from LocalCommChannel.
    Former-commit-id: 337790f19d0b3a65c2381d9b51d993d5476b1988

--- extensions/rmi/src/joliex/rmi/RemoteBasicChannel.java
+++ extensions/rmi/src/joliex/rmi/RemoteBasicChannel.java
@@ -33,7 +33,7 @@ import jolie.net.CommMessage;
 public interface RemoteBasicChannel extends Remote
 {
 	public CommMessage recvResponseFor( CommMessage request )
-		throws RemoteException;
+		throws RemoteException, IOException;
 	public CommMessage recv()
 		throws IOException;
 	public void send( CommMessage message )
