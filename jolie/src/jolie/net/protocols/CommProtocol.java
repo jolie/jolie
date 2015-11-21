2bef384e4717616536a4c578462f0f9a7783b7f6
Matthias Dieter Wallnöfer <mdw@samba.org>
2015-11-21 19:34:33 +0000

    CommProtocol.java - add missing "hasParameterValue()"
    Former-commit-id: aa041b9999d93fafe43eff45f5698cc969ac897b

--- jolie/src/jolie/net/protocols/CommProtocol.java
+++ jolie/src/jolie/net/protocols/CommProtocol.java
@@ -96,6 +96,15 @@ public abstract class CommProtocol
 		}
 		return false;
 	}
+
+	protected boolean hasParameterValue( String id )
+	{
+		if ( configurationPath.getValue().hasChildren( id ) ) {
+			Value v = configurationPath.getValue().getFirstChild( id );
+			return v.isDefined();
+		}
+		return false;
+	}
 	
 	/**
 	 * Shortcut for getParameterVector( id ).first()
