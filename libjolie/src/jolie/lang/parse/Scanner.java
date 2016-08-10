132b5668a23c374ad2c90e6b8765a15479042aba
fmontesi <famontesi@gmail.com>
2016-08-10 14:31:25 +0200

    Remove unused method Scanner.addStringTerminator

--- libjolie/src/jolie/lang/parse/Scanner.java
+++ libjolie/src/jolie/lang/parse/Scanner.java
@@ -398,12 +398,7 @@ public class Scanner
 		}
 		return tokenBuilder.toString();
 	}
-	
-	public static String addStringTerminator( String str )
-	{
-		return str + -1;
-	}
-	
+		
 	public InputStream inputStream()
 	{
 		return stream;
