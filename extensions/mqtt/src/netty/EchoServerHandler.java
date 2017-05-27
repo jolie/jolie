54b7fdc34db7a658ffeec797e44b7cc6927feaa6
Stefano Pio Zingaro <stefanopio.zingaro@unibo.it>
2017-05-27 16:29:12 +0200

    commit test from netbeans
    Former-commit-id: faa7c30971b11e26c83412407dc98481f5564aff

--- extensions/mqtt/src/netty/EchoServerHandler.java
+++ extensions/mqtt/src/netty/EchoServerHandler.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2017 stefanopiozingaro
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package netty;
 
 import io.netty.buffer.ByteBuf;
@@ -7,6 +23,10 @@ import io.netty.util.CharsetUtil;
 
 import java.text.MessageFormat;
 
+/**
+ *
+ * @author stefanopiozingaro
+ */
 @ChannelHandler.Sharable
 public class EchoServerHandler extends ChannelInboundHandlerAdapter {
 
@@ -14,7 +34,8 @@ public class EchoServerHandler extends ChannelInboundHandlerAdapter {
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
         // effettuo il casting di msg che è un oggetto generico
         ByteBuf buffer = (ByteBuf) msg;
-        System.out.println(MessageFormat.format("Il buffer di byte che è arrivato al server è: {0}"
+        System.out.println(MessageFormat.format("Il buffer di byte che è "
+                + "arrivato al server è: {0}"
                 , buffer.toString(CharsetUtil.UTF_8)));
     }
 
