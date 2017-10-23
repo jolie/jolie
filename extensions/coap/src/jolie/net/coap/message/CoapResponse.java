756b7ccd695fb698cf1f7a79969e6b5af0b06f03
Stefano Pio Zingaro <stefanopio.zingaro@unibo.it>
2017-10-23 18:15:21 +0200

    added support for dinamic URI path
    Former-commit-id: 6ef815b4cbff9f656ed15f80fd7401eb611a6023

--- extensions/coap/src/jolie/net/coap/message/CoapResponse.java
+++ extensions/coap/src/jolie/net/coap/message/CoapResponse.java
@@ -21,6 +21,7 @@
  */
 package jolie.net.coap.message;
 
+import io.netty.buffer.Unpooled;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
@@ -28,7 +29,9 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import java.util.Iterator;
+import static jolie.net.coap.message.CoapMessage.UNDEFINED_MESSAGE_ID;
 import jolie.net.coap.miscellaneous.BlockSize;
+import jolie.net.coap.miscellaneous.Token;
 import jolie.net.coap.options.OpaqueOptionValue;
 import jolie.net.coap.options.Option;
 import jolie.net.coap.options.OptionValue;
@@ -37,192 +40,193 @@ import jolie.net.coap.options.UintOptionValue;
 
 public class CoapResponse extends CoapMessage {
 
-    private static final String NO_ERRROR_CODE = "Code no. %s "
-	    + "is no error code!";
-    private static final long TEXT_PLAIN_UTF8 = 0;
-    private long MODULUS = (long) Math.pow(2, 24);
+  private static final String NO_ERRROR_CODE = "Code no. %s "
+      + "is no error code!";
+  private static final long TEXT_PLAIN_UTF8 = 0;
+  private long MODULUS = (long) Math.pow(2, 24);
 
-    public CoapResponse(int messageType, int messageCode)
-	    throws IllegalArgumentException {
+  public CoapResponse(int messageType, int messageCode)
+      throws IllegalArgumentException {
 
-	super(messageType, messageCode);
-	if (!MessageCode.isResponse(messageCode)) {
-	    throw new IllegalArgumentException("Message code no."
-		    + messageCode + " is no response code.");
-	}
+    super(messageType, messageCode, UNDEFINED_MESSAGE_ID,
+        new Token(new byte[0]));
+    if (!MessageCode.isResponse(messageCode)) {
+      throw new IllegalArgumentException("Message code no."
+          + messageCode + " is no response code.");
+    }
+  }
+
+  public static CoapResponse createErrorResponse(int messageType,
+      int messageCode, String content) throws IllegalArgumentException {
+
+    if (!MessageCode.isErrorMessage(messageCode)) {
+      throw new IllegalArgumentException(String.format(
+          NO_ERRROR_CODE, MessageCode.asString(messageCode)));
     }
 
-    public static CoapResponse createErrorResponse(int messageType,
-	    int messageCode, String content) throws IllegalArgumentException {
+    CoapResponse errorResponse = new CoapResponse(messageType, messageCode);
+    errorResponse.content(Unpooled.wrappedBuffer(
+        content.getBytes(CoapMessage.CHARSET)), TEXT_PLAIN_UTF8);
 
-	if (!MessageCode.isErrorMessage(messageCode)) {
-	    throw new IllegalArgumentException(String.format(
-		    NO_ERRROR_CODE, MessageCode.asString(messageCode)));
-	}
+    return errorResponse;
+  }
 
-	CoapResponse errorResponse = new CoapResponse(messageType, messageCode);
-	errorResponse.setContent(content.getBytes(CoapMessage.CHARSET),
-		TEXT_PLAIN_UTF8);
+  public static CoapResponse createErrorResponse(int messageType,
+      int messageCode, Throwable throwable)
+      throws IllegalArgumentException {
 
-	return errorResponse;
+    StringWriter stringWriter = new StringWriter();
+    throwable.printStackTrace(new PrintWriter(stringWriter));
+    return createErrorResponse(messageType, messageCode,
+        stringWriter.toString());
+  }
+
+  public boolean isErrorResponse() {
+    return MessageCode.isErrorMessage(this.getMessageCode());
+  }
+
+  public void setEtag(byte[] etag) throws IllegalArgumentException {
+    this.addOpaqueOption(Option.ETAG, etag);
+  }
+
+  public byte[] getEtag() {
+    if (options.containsKey(Option.ETAG)) {
+      return ((OpaqueOptionValue) options.get(Option.ETAG).iterator()
+          .next()).getDecodedValue();
+    } else {
+      return null;
+    }
+  }
+
+  public void setObserve() {
+    this.setObserve(System.currentTimeMillis() % MODULUS);
+  }
+
+  public void setPreferredBlock2Size(BlockSize block2Size) {
+    if (BlockSize.UNBOUND == block2Size || block2Size == null) {
+      this.removeOptions(Option.BLOCK_2);
+    } else {
+      this.setBlock2(0, false, block2Size.getSzx());
+    }
+  }
+
+  public void setBlock2(long number, boolean more, long szx)
+      throws IllegalArgumentException {
+    try {
+      this.removeOptions(Option.BLOCK_2);
+      if (number > 1048575) {
+        throw new IllegalArgumentException("Max. BLOCK2NUM is 1048575");
+      }
+      //long more = ((more) ? 1 : 0) << 3;
+      this.addUintOption(Option.BLOCK_2, ((number & 0xFFFFF) << 4)
+          + ((more ? 1 : 0) << 3) + szx);
+    } catch (IllegalArgumentException e) {
+      this.removeOptions(Option.BLOCK_2);
+    }
+  }
+
+  public void setBlock1(long number, long szx)
+      throws IllegalArgumentException {
+    try {
+      this.removeOptions(Option.BLOCK_1);
+      if (number > 1048575) {
+        throw new IllegalArgumentException("Max. BLOCK1NUM is 1048575");
+      }
+      //long more = ((more) ? 1 : 0) << 3;
+      this.addUintOption(Option.BLOCK_1, ((number & 0xFFFFF) << 4)
+          + (1 << 3) + szx);
+    } catch (IllegalArgumentException e) {
+      this.removeOptions(Option.BLOCK_1);
+    }
+  }
+
+  public boolean isUpdateNotification() {
+    return this.getObserve() != UintOptionValue.UNDEFINED;
+  }
+
+  public void setLocationURI(URI locationURI)
+      throws IllegalArgumentException {
+
+    options.remove(Option.LOCATION_PATH);
+    options.remove(Option.LOCATION_QUERY);
+
+    String locationPath = locationURI.getRawPath();
+    String locationQuery = locationURI.getRawQuery();
+
+    try {
+      if (locationPath != null) {
+        //Path must not start with "/" to be further processed
+        if (locationPath.startsWith("/")) {
+          locationPath = locationPath.substring(1);
+        }
+
+        for (String pathComponent : locationPath.split("/")) {
+          this.addStringOption(Option.LOCATION_PATH, pathComponent);
+        }
+      }
+
+      if (locationQuery != null) {
+        for (String queryComponent : locationQuery.split("&")) {
+          this.addStringOption(Option.LOCATION_QUERY, queryComponent);
+        }
+      }
+    } catch (IllegalArgumentException ex) {
+      options.remove(Option.LOCATION_PATH);
+      options.remove(Option.LOCATION_QUERY);
+      throw ex;
+    }
+  }
+
+  public URI getLocationURI() throws URISyntaxException {
+
+    //Reconstruct path
+    StringBuilder locationPath = new StringBuilder();
+
+    if (options.containsKey(Option.LOCATION_PATH)) {
+      for (OptionValue optionValue : options.get(Option.LOCATION_PATH)) {
+        locationPath.append("/")
+            .append(((StringOptionValue) optionValue)
+                .getDecodedValue());
+      }
     }
 
-    public static CoapResponse createErrorResponse(int messageType,
-	    int messageCode, Throwable throwable)
-	    throws IllegalArgumentException {
+    //Reconstruct query
+    StringBuilder locationQuery = new StringBuilder();
 
-	StringWriter stringWriter = new StringWriter();
-	throwable.printStackTrace(new PrintWriter(stringWriter));
-	return createErrorResponse(messageType, messageCode,
-		stringWriter.toString());
+    if (options.containsKey(Option.LOCATION_QUERY)) {
+      Iterator<OptionValue> queryComponentIterator
+          = options.get(Option.LOCATION_QUERY).iterator();
+      locationQuery.append(((StringOptionValue) queryComponentIterator
+          .next()).getDecodedValue());
+      while (queryComponentIterator.hasNext()) {
+        locationQuery.append("&")
+            .append(((StringOptionValue) queryComponentIterator
+                .next()).getDecodedValue());
+      }
     }
 
-    public boolean isErrorResponse() {
-	return MessageCode.isErrorMessage(this.getMessageCode());
+    if (locationPath.length() == 0 && locationQuery.length() == 0) {
+      return null;
     }
 
-    public void setEtag(byte[] etag) throws IllegalArgumentException {
-	this.addOpaqueOption(Option.ETAG, etag);
+    return new URI(null, null, null, (int) UintOptionValue.UNDEFINED,
+        locationPath.toString(), locationQuery.toString(), null);
+  }
+
+  public void setMaxAge(long maxAge) {
+    try {
+      this.options.remove(Option.MAX_AGE);
+      this.addUintOption(Option.MAX_AGE, maxAge);
+    } catch (IllegalArgumentException e) {
     }
+  }
 
-    public byte[] getEtag() {
-	if (options.containsKey(Option.ETAG)) {
-	    return ((OpaqueOptionValue) options.get(Option.ETAG).iterator()
-		    .next()).getDecodedValue();
-	} else {
-	    return null;
-	}
+  public long getMaxAge() {
+    if (options.containsKey(Option.MAX_AGE)) {
+      return ((UintOptionValue) options.get(Option.MAX_AGE).iterator()
+          .next()).getDecodedValue();
+    } else {
+      return OptionValue.MAX_AGE_DEFAULT;
     }
-
-    public void setObserve() {
-	this.setObserve(System.currentTimeMillis() % MODULUS);
-    }
-
-    public void setPreferredBlock2Size(BlockSize block2Size) {
-	if (BlockSize.UNBOUND == block2Size || block2Size == null) {
-	    this.removeOptions(Option.BLOCK_2);
-	} else {
-	    this.setBlock2(0, false, block2Size.getSzx());
-	}
-    }
-
-    public void setBlock2(long number, boolean more, long szx)
-	    throws IllegalArgumentException {
-	try {
-	    this.removeOptions(Option.BLOCK_2);
-	    if (number > 1048575) {
-		throw new IllegalArgumentException("Max. BLOCK2NUM is 1048575");
-	    }
-	    //long more = ((more) ? 1 : 0) << 3;
-	    this.addUintOption(Option.BLOCK_2, ((number & 0xFFFFF) << 4)
-		    + ((more ? 1 : 0) << 3) + szx);
-	} catch (IllegalArgumentException e) {
-	    this.removeOptions(Option.BLOCK_2);
-	}
-    }
-
-    public void setBlock1(long number, long szx)
-	    throws IllegalArgumentException {
-	try {
-	    this.removeOptions(Option.BLOCK_1);
-	    if (number > 1048575) {
-		throw new IllegalArgumentException("Max. BLOCK1NUM is 1048575");
-	    }
-	    //long more = ((more) ? 1 : 0) << 3;
-	    this.addUintOption(Option.BLOCK_1, ((number & 0xFFFFF) << 4)
-		    + (1 << 3) + szx);
-	} catch (IllegalArgumentException e) {
-	    this.removeOptions(Option.BLOCK_1);
-	}
-    }
-
-    public boolean isUpdateNotification() {
-	return this.getObserve() != UintOptionValue.UNDEFINED;
-    }
-
-    public void setLocationURI(URI locationURI)
-	    throws IllegalArgumentException {
-
-	options.remove(Option.LOCATION_PATH);
-	options.remove(Option.LOCATION_QUERY);
-
-	String locationPath = locationURI.getRawPath();
-	String locationQuery = locationURI.getRawQuery();
-
-	try {
-	    if (locationPath != null) {
-		//Path must not start with "/" to be further processed
-		if (locationPath.startsWith("/")) {
-		    locationPath = locationPath.substring(1);
-		}
-
-		for (String pathComponent : locationPath.split("/")) {
-		    this.addStringOption(Option.LOCATION_PATH, pathComponent);
-		}
-	    }
-
-	    if (locationQuery != null) {
-		for (String queryComponent : locationQuery.split("&")) {
-		    this.addStringOption(Option.LOCATION_QUERY, queryComponent);
-		}
-	    }
-	} catch (IllegalArgumentException ex) {
-	    options.remove(Option.LOCATION_PATH);
-	    options.remove(Option.LOCATION_QUERY);
-	    throw ex;
-	}
-    }
-
-    public URI getLocationURI() throws URISyntaxException {
-
-	//Reconstruct path
-	StringBuilder locationPath = new StringBuilder();
-
-	if (options.containsKey(Option.LOCATION_PATH)) {
-	    for (OptionValue optionValue : options.get(Option.LOCATION_PATH)) {
-		locationPath.append("/")
-			.append(((StringOptionValue) optionValue)
-				.getDecodedValue());
-	    }
-	}
-
-	//Reconstruct query
-	StringBuilder locationQuery = new StringBuilder();
-
-	if (options.containsKey(Option.LOCATION_QUERY)) {
-	    Iterator<OptionValue> queryComponentIterator
-		    = options.get(Option.LOCATION_QUERY).iterator();
-	    locationQuery.append(((StringOptionValue) queryComponentIterator
-		    .next()).getDecodedValue());
-	    while (queryComponentIterator.hasNext()) {
-		locationQuery.append("&")
-			.append(((StringOptionValue) queryComponentIterator
-				.next()).getDecodedValue());
-	    }
-	}
-
-	if (locationPath.length() == 0 && locationQuery.length() == 0) {
-	    return null;
-	}
-
-	return new URI(null, null, null, (int) UintOptionValue.UNDEFINED,
-		locationPath.toString(), locationQuery.toString(), null);
-    }
-
-    public void setMaxAge(long maxAge) {
-	try {
-	    this.options.remove(Option.MAX_AGE);
-	    this.addUintOption(Option.MAX_AGE, maxAge);
-	} catch (IllegalArgumentException e) {
-	}
-    }
-
-    public long getMaxAge() {
-	if (options.containsKey(Option.MAX_AGE)) {
-	    return ((UintOptionValue) options.get(Option.MAX_AGE).iterator()
-		    .next()).getDecodedValue();
-	} else {
-	    return OptionValue.MAX_AGE_DEFAULT;
-	}
-    }
+  }
 }
