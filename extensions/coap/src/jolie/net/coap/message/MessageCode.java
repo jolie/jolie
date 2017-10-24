c70faf4a0110ccd6d92d29971718a11138424b08
Stefano Pio Zingaro <stefanopio.zingaro@unibo.it>
2017-10-24 17:41:39 +0200

    remove abstractions of message package

--- extensions/coap/src/jolie/net/coap/message/MessageCode.java
+++ extensions/coap/src/jolie/net/coap/message/MessageCode.java
@@ -23,125 +23,125 @@ package jolie.net.coap.message;
 
 import java.util.HashMap;
 
-public abstract class MessageCode {
+public class MessageCode {
 
-    public static final int EMPTY = 0;
-    public static final int GET = 1;
-    public static final int POST = 2;
-    public static final int PUT = 3;
-    public static final int DELETE = 4;
-    public static final int CREATED_201 = 65;
-    public static final int DELETED_202 = 66;
-    public static final int VALID_203 = 67;
-    public static final int CHANGED_204 = 68;
-    public static final int CONTENT_205 = 69;
-    public static final int CONTINUE_231 = 95;
-    public static final int BAD_REQUEST_400 = 128;
-    public static final int UNAUTHORIZED_401 = 129;
-    public static final int BAD_OPTION_402 = 130;
-    public static final int FORBIDDEN_403 = 131;
-    public static final int NOT_FOUND_404 = 132;
-    public static final int METHOD_NOT_ALLOWED_405 = 133;
-    public static final int NOT_ACCEPTABLE_406 = 134;
-    public static final int REQUEST_ENTITY_INCOMPLETE_408 = 136;
-    public static final int PRECONDITION_FAILED_412 = 140;
-    public static final int REQUEST_ENTITY_TOO_LARGE_413 = 141;
-    public static final int UNSUPPORTED_CONTENT_FORMAT_415 = 143;
-    public static final int INTERNAL_SERVER_ERROR_500 = 160;
-    public static final int NOT_IMPLEMENTED_501 = 161;
-    public static final int BAD_GATEWAY_502 = 162;
-    public static final int SERVICE_UNAVAILABLE_503 = 163;
-    public static final int GATEWAY_TIMEOUT_504 = 164;
-    public static final int PROXYING_NOT_SUPPORTED_505 = 165;
+  public static final int EMPTY = 0;
+  public static final int GET = 1;
+  public static final int POST = 2;
+  public static final int PUT = 3;
+  public static final int DELETE = 4;
+  public static final int CREATED_201 = 65;
+  public static final int DELETED_202 = 66;
+  public static final int VALID_203 = 67;
+  public static final int CHANGED_204 = 68;
+  public static final int CONTENT_205 = 69;
+  public static final int CONTINUE_231 = 95;
+  public static final int BAD_REQUEST_400 = 128;
+  public static final int UNAUTHORIZED_401 = 129;
+  public static final int BAD_OPTION_402 = 130;
+  public static final int FORBIDDEN_403 = 131;
+  public static final int NOT_FOUND_404 = 132;
+  public static final int METHOD_NOT_ALLOWED_405 = 133;
+  public static final int NOT_ACCEPTABLE_406 = 134;
+  public static final int REQUEST_ENTITY_INCOMPLETE_408 = 136;
+  public static final int PRECONDITION_FAILED_412 = 140;
+  public static final int REQUEST_ENTITY_TOO_LARGE_413 = 141;
+  public static final int UNSUPPORTED_CONTENT_FORMAT_415 = 143;
+  public static final int INTERNAL_SERVER_ERROR_500 = 160;
+  public static final int NOT_IMPLEMENTED_501 = 161;
+  public static final int BAD_GATEWAY_502 = 162;
+  public static final int SERVICE_UNAVAILABLE_503 = 163;
+  public static final int GATEWAY_TIMEOUT_504 = 164;
+  public static final int PROXYING_NOT_SUPPORTED_505 = 165;
 
-    private static final HashMap<Integer, String> MESSAGE_CODES
-	    = new HashMap<>();
+  private static final HashMap<Integer, String> MESSAGE_CODES
+      = new HashMap<>();
 
-    static {
+  static {
 
-	MESSAGE_CODES.put(EMPTY, "EMPTY");
-	MESSAGE_CODES.put(GET, "GET");
-	MESSAGE_CODES.put(POST, "POST");
-	MESSAGE_CODES.put(PUT, "PUT");
-	MESSAGE_CODES.put(DELETE, "DELETE");
-	MESSAGE_CODES.put(CREATED_201, "CREATED");
-	MESSAGE_CODES.put(DELETED_202, "DELETED");
-	MESSAGE_CODES.put(VALID_203, "VALID");
-	MESSAGE_CODES.put(CHANGED_204, "CHANGED");
-	MESSAGE_CODES.put(CONTENT_205, "CONTENT");
-	MESSAGE_CODES.put(CONTINUE_231, "CONTINUE");
-	MESSAGE_CODES.put(BAD_REQUEST_400, "BAD REQUEST");
-	MESSAGE_CODES.put(UNAUTHORIZED_401, "UNAUTHORIZED");
-	MESSAGE_CODES.put(BAD_OPTION_402, "BAD OPTION");
-	MESSAGE_CODES.put(FORBIDDEN_403, "FORBIDDEN");
-	MESSAGE_CODES.put(NOT_FOUND_404, "NOT FOUND");
-	MESSAGE_CODES.put(METHOD_NOT_ALLOWED_405, "METHOD NOT ALLOWED");
-	MESSAGE_CODES.put(NOT_ACCEPTABLE_406, "NOT ACCEPTABLE");
-	MESSAGE_CODES.put(REQUEST_ENTITY_INCOMPLETE_408, "REQUEST ENTITY INCOMPLETE");
-	MESSAGE_CODES.put(PRECONDITION_FAILED_412, "PRECONDITION FAILED");
-	MESSAGE_CODES.put(REQUEST_ENTITY_TOO_LARGE_413, "REQUEST ENTITY TOO LARGE");
-	MESSAGE_CODES.put(UNSUPPORTED_CONTENT_FORMAT_415, "UNSUPPORTED CONTENT FORMAT");
-	MESSAGE_CODES.put(INTERNAL_SERVER_ERROR_500, "INTERNAL SERVER ERROR");
-	MESSAGE_CODES.put(NOT_IMPLEMENTED_501, "NOT IMPLEMENTED");
-	MESSAGE_CODES.put(BAD_GATEWAY_502, "BAD GATEWAY");
-	MESSAGE_CODES.put(SERVICE_UNAVAILABLE_503, "SERVICE UNAVAILABLE");
-	MESSAGE_CODES.put(GATEWAY_TIMEOUT_504, "GATEWAY TIMEOUT");
-	MESSAGE_CODES.put(PROXYING_NOT_SUPPORTED_505, "PROXYING NOT SUPPORTED");
-    }
+    MESSAGE_CODES.put(EMPTY, "EMPTY");
+    MESSAGE_CODES.put(GET, "GET");
+    MESSAGE_CODES.put(POST, "POST");
+    MESSAGE_CODES.put(PUT, "PUT");
+    MESSAGE_CODES.put(DELETE, "DELETE");
+    MESSAGE_CODES.put(CREATED_201, "CREATED");
+    MESSAGE_CODES.put(DELETED_202, "DELETED");
+    MESSAGE_CODES.put(VALID_203, "VALID");
+    MESSAGE_CODES.put(CHANGED_204, "CHANGED");
+    MESSAGE_CODES.put(CONTENT_205, "CONTENT");
+    MESSAGE_CODES.put(CONTINUE_231, "CONTINUE");
+    MESSAGE_CODES.put(BAD_REQUEST_400, "BAD REQUEST");
+    MESSAGE_CODES.put(UNAUTHORIZED_401, "UNAUTHORIZED");
+    MESSAGE_CODES.put(BAD_OPTION_402, "BAD OPTION");
+    MESSAGE_CODES.put(FORBIDDEN_403, "FORBIDDEN");
+    MESSAGE_CODES.put(NOT_FOUND_404, "NOT FOUND");
+    MESSAGE_CODES.put(METHOD_NOT_ALLOWED_405, "METHOD NOT ALLOWED");
+    MESSAGE_CODES.put(NOT_ACCEPTABLE_406, "NOT ACCEPTABLE");
+    MESSAGE_CODES.put(REQUEST_ENTITY_INCOMPLETE_408, "REQUEST ENTITY INCOMPLETE");
+    MESSAGE_CODES.put(PRECONDITION_FAILED_412, "PRECONDITION FAILED");
+    MESSAGE_CODES.put(REQUEST_ENTITY_TOO_LARGE_413, "REQUEST ENTITY TOO LARGE");
+    MESSAGE_CODES.put(UNSUPPORTED_CONTENT_FORMAT_415, "UNSUPPORTED CONTENT FORMAT");
+    MESSAGE_CODES.put(INTERNAL_SERVER_ERROR_500, "INTERNAL SERVER ERROR");
+    MESSAGE_CODES.put(NOT_IMPLEMENTED_501, "NOT IMPLEMENTED");
+    MESSAGE_CODES.put(BAD_GATEWAY_502, "BAD GATEWAY");
+    MESSAGE_CODES.put(SERVICE_UNAVAILABLE_503, "SERVICE UNAVAILABLE");
+    MESSAGE_CODES.put(GATEWAY_TIMEOUT_504, "GATEWAY TIMEOUT");
+    MESSAGE_CODES.put(PROXYING_NOT_SUPPORTED_505, "PROXYING NOT SUPPORTED");
+  }
 
-    /**
-     *
-     * @param messageCode
-     * @return String
-     */
-    public static String asString(int messageCode) {
-	String result = MESSAGE_CODES.get(messageCode);
-	return result == null ? "UNKOWN" : result;
-    }
+  /**
+   *
+   * @param messageCode
+   * @return String
+   */
+  public static String asString(int messageCode) {
+    String result = MESSAGE_CODES.get(messageCode);
+    return result == null ? "UNKOWN" : result;
+  }
 
-    /**
-     *
-     * @param number
-     * @return boolean
-     */
-    public static boolean isMessageCode(int number) {
-	return MESSAGE_CODES.containsKey(number);
-    }
+  /**
+   *
+   * @param number
+   * @return boolean
+   */
+  public static boolean isMessageCode(int number) {
+    return MESSAGE_CODES.containsKey(number);
+  }
 
-    /**
-     *
-     * @param messageCode
-     * @return boolean
-     */
-    public static boolean isRequest(int messageCode) {
-	return (messageCode > 0 && messageCode < 5);
-    }
+  /**
+   *
+   * @param messageCode
+   * @return boolean
+   */
+  public static boolean isRequest(int messageCode) {
+    return (messageCode > 0 && messageCode < 5);
+  }
 
-    /**
-     *
-     * @param messageCode
-     * @return boolean
-     */
-    public static boolean isResponse(int messageCode) {
-	return messageCode >= 5;
-    }
+  /**
+   *
+   * @param messageCode
+   * @return boolean
+   */
+  public static boolean isResponse(int messageCode) {
+    return messageCode >= 5;
+  }
 
-    /**
-     *
-     * @param codeNumber
-     * @return boolean
-     */
-    public static boolean isErrorMessage(int codeNumber) {
-	return (codeNumber >= 128);
-    }
+  /**
+   *
+   * @param codeNumber
+   * @return boolean
+   */
+  public static boolean isErrorMessage(int codeNumber) {
+    return (codeNumber >= 128);
+  }
 
-    /**
-     *
-     * @param codeNumber
-     * @return boolean
-     */
-    public static boolean allowsContent(int codeNumber) {
-	return !(codeNumber == GET || codeNumber == DELETE);
-    }
+  /**
+   *
+   * @param codeNumber
+   * @return boolean
+   */
+  public static boolean allowsContent(int codeNumber) {
+    return !(codeNumber == GET || codeNumber == DELETE);
+  }
 
 }
--- extensions/coap/src/jolie/net/coap/message/MessageType.java
+++ extensions/coap/src/jolie/net/coap/message/MessageType.java
@@ -23,40 +23,40 @@ package jolie.net.coap.message;
 
 import java.util.HashMap;
 
-public abstract class MessageType {
+public class MessageType {
 
-    public static final int CON = 0;
-    public static final int NON = 1;
-    public static final int ACK = 2;
-    public static final int RST = 3;
+  public static final int CON = 0;
+  public static final int NON = 1;
+  public static final int ACK = 2;
+  public static final int RST = 3;
 
-    private static final HashMap<Integer, String> MESSAGE_TYPES
-	    = new HashMap<>();
+  private static final HashMap<Integer, String> MESSAGE_TYPES
+      = new HashMap<>();
 
-    static {
-	MESSAGE_TYPES.put(CON, "CON");
-	MESSAGE_TYPES.put(NON, "NON");
-	MESSAGE_TYPES.put(ACK, "ACK");
-	MESSAGE_TYPES.put(RST, "RST");
-    }
+  static {
+    MESSAGE_TYPES.put(CON, "CON");
+    MESSAGE_TYPES.put(NON, "NON");
+    MESSAGE_TYPES.put(ACK, "ACK");
+    MESSAGE_TYPES.put(RST, "RST");
+  }
 
-    /**
-     *
-     * @param messageType
-     * @return String
-     */
-    public static String asString(int messageType) {
-	String result = MESSAGE_TYPES.get(messageType);
-	return result == null ? "UNKOWN (" + messageType + ")" : result;
-    }
+  /**
+   *
+   * @param messageType
+   * @return String
+   */
+  public static String asString(int messageType) {
+    String result = MESSAGE_TYPES.get(messageType);
+    return result == null ? "UNKOWN (" + messageType + ")" : result;
+  }
 
-    /**
-     *
-     * @param number
-     * @return boolean
-     */
-    public static boolean isMessageType(int number) {
-	return MESSAGE_TYPES.containsKey(number);
-    }
+  /**
+   *
+   * @param number
+   * @return boolean
+   */
+  public static boolean isMessageType(int number) {
+    return MESSAGE_TYPES.containsKey(number);
+  }
 
 }
