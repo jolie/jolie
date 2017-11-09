c0fc1db12875b0768db1f19fbac03719bce120db
Stefano Pio Zingaro <stefanopio.zingaro@unibo.it>
2017-11-09 19:46:26 +0100

    datagram decoder handler added
    Former-commit-id: b17f391104fefc18dc73432784bec3c3e3cce095

---
+++ extensions/datagram/src/jolie/net/DatagramPacketDecoder.java
@@ -0,0 +1,80 @@
+/*
+ *   Copyright (C) 2017 by Stefano Pio Zingaro <stefanopio.zingaro@unibo.it>  
+ *   Copyright (C) 2017 by Saverio Giallorenzo <saverio.giallorenzo@gmail.com>
+ *                                                                             
+ *   This program is free software; you can redistribute it and/or modify      
+ *   it under the terms of the GNU Library General Public License as           
+ *   published by the Free Software Foundation; either version 2 of the        
+ *   License, or (at your option) any later version.                           
+ *                                                                             
+ *   This program is distributed in the hope that it will be useful,           
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of            
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             
+ *   GNU General Public License for more details.                              
+ *                                                                             
+ *   You should have received a copy of the GNU Library General Public         
+ *   License along with this program; if not, write to the                     
+ *   Free Software Foundation, Inc.,                                           
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.                 
+ *                                                                             
+ *   For details about the authors of this software, see the AUTHORS file.     
+ */
+package jolie.net;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.ByteBufUtil;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.socket.DatagramPacket;
+import io.netty.handler.codec.MessageToMessageEncoder;
+import io.netty.util.ReferenceCounted;
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.util.List;
+
+class DatagramPacketDecoder extends MessageToMessageEncoder<ByteBuf> {
+
+  private URI location;
+
+  DatagramPacketDecoder(URI location) {
+    this.location = location;
+  }
+
+  @Override
+  protected void encode(ChannelHandlerContext ctx, ByteBuf msg,
+      List<Object> out) throws Exception {
+
+    DatagramPacket dp;
+    InetSocketAddress recipient = new InetSocketAddress(this.location.getHost(),
+        this.location.getPort());
+    if (msg.isDirect() && msg.nioBufferCount() == 1) {
+      dp = new DatagramPacket(msg, recipient);
+    } else {
+      ReferenceCounted p = null;
+      dp = new DatagramPacket(newDirectBuffer(msg), recipient);
+    }
+  }
+
+  protected final ByteBuf newDirectBuffer(ByteBuf buf) {
+    final int readableBytes = buf.readableBytes();
+    if (readableBytes == 0) {
+      return Unpooled.EMPTY_BUFFER;
+    }
+
+    final ByteBufAllocator alloc = ByteBufAllocator.DEFAULT;
+    if (alloc.isDirectBufferPooled()) {
+      ByteBuf directBuf = alloc.directBuffer(readableBytes);
+      directBuf.writeBytes(buf, buf.readerIndex(), readableBytes);
+      return directBuf;
+    }
+
+    final ByteBuf directBuf = ByteBufUtil.threadLocalDirectBuffer();
+    if (directBuf != null) {
+      directBuf.writeBytes(buf, buf.readerIndex(), readableBytes);
+      return directBuf;
+    }
+
+    return buf;
+  }
+}
