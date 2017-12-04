c6f8669ad8aa4da6a6b7c0f2a5a5818e4a14f100
Stefano Pio Zingaro <stefanopio.zingaro@unibo.it>
2017-12-04 11:22:59 +0100

    changed package

--- extensions/coap/src/jolie/net/coap/exception/HeaderDecodingException.java
+++ extensions/coap/src/jolie/net/coap/exception/HeaderDecodingException.java
@@ -19,7 +19,7 @@
  *                                                                             
  *   For details about the authors of this software, see the AUTHORS file.     
  */
-package jolie.net.coap;
+package jolie.net.coap.exception;
 
 import java.net.InetSocketAddress;
 
