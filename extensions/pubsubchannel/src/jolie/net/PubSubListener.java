a715342ddbff9b26b9bd81fba3e41f1efc23e091
Stefano Pio Zingaro <stefanopio.zingaro@unibo.it>
2017-12-28 21:51:51 +0100

    pub sub channel minor fixes

--- extensions/pubsubchannel/src/jolie/net/PubSubListener.java
+++ extensions/pubsubchannel/src/jolie/net/PubSubListener.java
@@ -1,6 +1,6 @@
 /*****************************************************************************
  * Copyright (C) 2017 by Saverio Giallorenzo <saverio.giallorenzo@gmail.com> *
- *                                                                           *
+ * Copyright (C) 2017 by Stefano Pio Zingaro <stefanopio.zingaro@unibo.it>                                                                          *
  *                                                                           *
  *   This program is free software; you can redistribute it and/or modify    *
  *   it under the terms of the GNU Library General Public License as         *  
