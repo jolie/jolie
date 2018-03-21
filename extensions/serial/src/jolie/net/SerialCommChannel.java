5d8db30988471f7a56cb63310ed74c95a11edfe6
Stefano Pio Zingaro <stefanopio.zingaro@unibo.it>
2018-03-21 14:43:15 +0100

    serial progress
    Former-commit-id: d24d7edf3472c688dfd1db52d6b9bced813e946b

---
+++ extensions/serial/src/jolie/net/SerialCommChannel.java
@@ -0,0 +1,219 @@
+/*
+ *   Copyright (C) 2017 by Stefano Pio Zingaro <stefanopio.zingaro@unibo.it>  
+ *   Copyright (C) 2017 by Saverio Giallorenzo <saverio.giallorenzo@gmail.com>
+ *                                                                             
+ *   This program is free software; you can redistribute it and/or modify      
+ *   it under the terms of the GNU Library General Public License as           
+ *   published by the Free Software Foundation; either version 2 of the        
+ *   License, or (at your option) any later version.                           
+ *                                                                             
+ *   This program is distributed in the hope that it will be useful,           
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of            
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             
+ *   GNU General Public License for more details.                              
+ *                                                                             
+ *   You should have received a copy of the GNU Library General Public         
+ *   License along with this program; if not, write to the                     
+ *   Free Software Foundation, Inc.,                                           
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.                 
+ *                                                                             
+ *   For details about the authors of this software, see the AUTHORS file.     
+ */
+package jolie.net;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import jolie.ExecutionThread;
+import jolie.net.protocols.AsyncCommProtocol;
+import jolie.net.ports.InputPort;
+import jolie.net.ports.OutputPort;
+import jolie.net.ports.Port;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.socket.DatagramPacket;
+import io.netty.channel.socket.nio.NioDatagramChannel;
+import jolie.Interpreter;
+
+public class SerialCommChannel extends StreamingCommChannel {
+
+	public final static String CHANNEL_HANDLER_NAME
+		= "STREAMING-CHANNEL-HANDLER";
+
+	private Bootstrap b;
+	protected CompletableFuture<CommMessage> waitingForMsg = null;
+	protected StreamingCommChannelHandler commChannelHandler;
+	private ChannelPipeline channelPipeline;
+
+	public SerialCommChannel( URI location, AsyncCommProtocol protocol ) {
+		super( location, protocol );
+		this.commChannelHandler = new StreamingCommChannelHandler( this );
+	}
+
+	@Override
+	public StreamingCommChannelHandler getChannelHandler() {
+		return commChannelHandler;
+	}
+
+	/**
+	 *
+	 * @param channelPipeline
+	 */
+	public void setChannelPipeline( ChannelPipeline channelPipeline ) {
+		this.channelPipeline = channelPipeline;
+	}
+
+	/**
+	 *
+	 * @return
+	 */
+	public ChannelPipeline getChannelPipeline() {
+		return channelPipeline;
+	}
+
+	/**
+	 *
+	 * @param location
+	 * @param protocol
+	 * @param workerGroup
+	 * @param port
+	 * @return
+	 */
+	public static SerialCommChannel createChannel( URI location,
+		AsyncCommProtocol protocol, EventLoopGroup workerGroup, Port port ) {
+
+//    ExecutionThread ethread = ExecutionThread.currentThread();
+		SerialCommChannel c = new SerialCommChannel( location, protocol );
+
+		c.b = new Bootstrap();
+		c.b.group( workerGroup );
+		c.b.channel( NioDatagramChannel.class );
+		c.b.handler( new ChannelInitializer<NioDatagramChannel>() {
+			@Override
+			protected void initChannel( NioDatagramChannel ch ) throws Exception {
+				ChannelPipeline p = ch.pipeline();
+				if ( port instanceof InputPort ) {
+					c.setParentInputPort( ( InputPort ) port );
+				}
+				if ( port instanceof OutputPort ) {
+					c.setParentOutputPort( ( OutputPort ) port );
+				}
+				protocol.setChannel( c );
+				c.setChannelPipeline( p );
+				protocol.setupPipeline( p );
+				p.addLast( CHANNEL_HANDLER_NAME, c.commChannelHandler );
+				p.addFirst( "BYTE-BUF-FORWARDER", new SimpleChannelInboundHandler<DatagramPacket>() {
+
+					@Override
+					public void channelRegistered( ChannelHandlerContext ctx ) throws Exception {
+						if ( port instanceof InputPort ) {
+							SerialListener.addResponseChannel();
+						}
+						super.channelRegistered( ctx );
+					}
+
+					@Override
+					protected void channelRead0( ChannelHandlerContext chc, DatagramPacket i )
+						throws Exception {
+						chc.fireChannelRead( i.content().retain() );
+					}
+
+					@Override
+					public void exceptionCaught( ChannelHandlerContext ctx, Throwable cause )
+						throws Exception {
+						Interpreter.getInstance().logSevere( cause );
+						ctx.close();
+					}
+
+				} );
+//        ch.attr(NioSocketCommChannel.EXECUTION_CONTEXT).set(ethread);
+			}
+		} );
+
+		return c;
+	}
+
+	/**
+	 *
+	 * @param location
+	 * @return
+	 * @throws InterruptedException
+	 */
+	public ChannelFuture connect( URI location )
+		throws InterruptedException {
+
+		return b.bind( new InetSocketAddress( 0 ) );
+	}
+
+	/**
+	 *
+	 * @param location
+	 * @return
+	 * @throws InterruptedException
+	 */
+	public ChannelFuture bind( InetSocketAddress location )
+		throws InterruptedException {
+
+		return b.bind( location );
+	}
+
+	/**
+	 * This is blocking to integrate with existing CommCore and ExecutionThreads.
+	 *
+	 * @return
+	 * @throws IOException
+	 */
+	@Override
+	protected CommMessage recvImpl() throws IOException {
+		try {
+			if ( waitingForMsg != null ) {
+				throw new UnsupportedOperationException( "Waiting for multiple "
+					+ "messages is currently not supported!" );
+			}
+			waitingForMsg = new CompletableFuture<>();
+			CommMessage msg = waitingForMsg.get();
+			waitingForMsg = null;
+			return msg;
+		} catch ( InterruptedException | ExecutionException ex ) {
+			throw new IOException( ex );
+		}
+	}
+
+	protected void completeRead( CommMessage message ) {
+		while ( waitingForMsg == null ) {
+			// spinlock
+		}
+		if ( waitingForMsg == null ) {
+			throw new IllegalStateException( "No pending read to complete!" );
+		} else {
+			waitingForMsg.complete( message );
+		}
+	}
+
+	@Override
+	protected void sendImpl( CommMessage message ) throws IOException {
+		try {
+			commChannelHandler.write( message.setExecutionThread( ExecutionThread.currentThread() ) ).sync();
+		} catch ( InterruptedException ex ) {
+			throw new IOException( ex );
+		}
+	}
+
+	@Override
+	protected void closeImpl() throws IOException {
+		try {
+			commChannelHandler.close().sync();
+		} catch ( InterruptedException ex ) {
+			throw new IOException( ex );
+		}
+	}
+}
---
+++ extensions/serial/src/jolie/net/SerialListener.java
@@ -0,0 +1,140 @@
+/*
+ *   Copyright (C) 2017 by Stefano Pio Zingaro <stefanopio.zingaro@unibo.it>  
+ *   Copyright (C) 2017 by Saverio Giallorenzo <saverio.giallorenzo@gmail.com>
+ *                                                                             
+ *   This program is free software; you can redistribute it and/or modify      
+ *   it under the terms of the GNU Library General Public License as           
+ *   published by the Free Software Foundation; either version 2 of the        
+ *   License, or (at your option) any later version.                           
+ *                                                                             
+ *   This program is distributed in the hope that it will be useful,           
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of            
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             
+ *   GNU General Public License for more details.                              
+ *                                                                             
+ *   You should have received a copy of the GNU Library General Public         
+ *   License along with this program; if not, write to the                     
+ *   Free Software Foundation, Inc.,                                           
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.                 
+ *                                                                             
+ *   For details about the authors of this software, see the AUTHORS file.     
+ */
+package jolie.net;
+
+import jolie.Interpreter;
+
+import jolie.net.ext.CommProtocolFactory;
+import jolie.net.ports.InputPort;
+import jolie.net.protocols.AsyncCommProtocol;
+import jolie.net.protocols.CommProtocol;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelOutboundHandlerAdapter;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.socket.DatagramPacket;
+import io.netty.channel.socket.nio.NioDatagramChannel;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class SerialListener extends CommListener {
+
+	private final EventLoopGroup workerGroup;
+	private final InetSocketAddress localAddress;
+	private Channel clientChannel;
+	private Channel serverChannel;
+	public static final ReentrantReadWriteLock responseChannels
+		= new ReentrantReadWriteLock();
+
+	public SerialListener(
+		Interpreter interpreter,
+		CommProtocolFactory protocolFactory,
+		InputPort inputPort,
+		EventLoopGroup workerGroup
+	) {
+		super( interpreter, protocolFactory, inputPort );
+		this.workerGroup = workerGroup;
+		this.localAddress = new InetSocketAddress( inputPort().location().getHost(),
+			inputPort().location().getPort() );
+	}
+
+	@Override
+	public void shutdown() {
+		if ( serverChannel.isOpen() ) {
+//      System.out.println("--- PRENDO WRITE LOCK --- " + Thread.currentThread());
+			responseChannels.writeLock().lock();
+			try {
+//        System.out.println("!!! Canale Chiuso !!!");
+				serverChannel.close();
+			} finally {
+//        System.out.println("--- RILASCIO WRITE LOCK --- " + Thread.currentThread());
+				responseChannels.writeLock().unlock();
+			}
+		}
+	}
+
+	public static void addResponseChannel() {
+//    System.out.println("--- PRENDO READ LOCK --- " + Thread.currentThread());
+		responseChannels.readLock().lock();
+	}
+
+	public static void removeResponseChannel() {
+//    System.out.println("--- RILASCIO READ LOCK --- " + Thread.currentThread());
+		responseChannels.readLock().unlock();
+	}
+
+	@Override
+	public void run() {
+
+		Bootstrap b = new Bootstrap();
+		b.group( workerGroup ).channel( NioDatagramChannel.class );
+		b.localAddress( localAddress );
+		b.handler( new SimpleChannelInboundHandler<DatagramPacket>() {
+
+			@Override
+			protected void channelRead0( ChannelHandlerContext ctx, DatagramPacket msg )
+				throws Exception {
+
+				CommProtocol protocol = createProtocol();
+				if ( !( protocol instanceof AsyncCommProtocol ) ) {
+					throw new UnsupportedCommProtocolException( "Use an async protocol" );
+				}
+
+				SerialCommChannel commChannel = SerialCommChannel.createChannel(
+					null,
+					( AsyncCommProtocol ) protocol,
+					workerGroup,
+					inputPort()
+				);
+
+				ChannelFuture f = commChannel.bind( new InetSocketAddress( 0 ) ).sync();
+				clientChannel = f.channel();
+
+				clientChannel.pipeline().addFirst( "FLUSHER", new ChannelOutboundHandlerAdapter() {
+
+					@Override
+					public void flush( ChannelHandlerContext ctx )
+						throws Exception {
+						removeResponseChannel();
+						super.flush( ctx );
+					}
+				} );
+				clientChannel.pipeline().fireChannelRead( msg.content().retain() );
+			}
+		} );
+
+		try {
+			ChannelFuture f = b.bind().sync();
+			serverChannel = f.channel();
+			serverChannel.closeFuture().sync();
+		} catch ( InterruptedException ex ) {
+			Interpreter.getInstance().logWarning( ex );
+		} finally {
+			workerGroup.shutdownGracefully();
+		}
+	}
+}
