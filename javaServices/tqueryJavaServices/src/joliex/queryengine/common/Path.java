5e2c907934e968b629334eaa53d062273902fb1a
thesave <thesave86@gmail.com>
2018-12-14 11:42:20 +0100

    licenses

--- javaServices/tqueryJavaServices/src/joliex/queryengine/common/Path.java
+++ javaServices/tqueryJavaServices/src/joliex/queryengine/common/Path.java
@@ -1,80 +1,74 @@
-/*
- *   Copyright (C) 2018 by Stefano Pio Zingaro <stefanopio.zingaro@unibo.it>
- *   Copyright (C) 2018 by Saverio Giallorenzo <saverio.giallorenzo@gmail.com>
- *
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU Library General Public License as
- *   published by the Free Software Foundation; either version 2 of the
- *   License, or (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU Library General Public
- *   License along with this program; if not, write to the
- *   Free Software Foundation, Inc.,
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- *   For details about the authors of this software, see the AUTHORS file.
- */
+/*******************************************************************************
+ *   Copyright (C) 2018 by Larisa Safina <safina@imada.sdu.dk>                 *
+ *   Copyright (C) 2018 by Stefano Pio Zingaro <stefanopio.zingaro@unibo.it>   *
+ *   Copyright (C) 2018 by Saverio Giallorenzo <saverio.giallorenzo@gmail.com> *
+ *                                                                             *
+ *   This program is free software; you can redistribute it and/or modify      *
+ *   it under the terms of the GNU Library General Public License as           *
+ *   published by the Free Software Foundation; either version 2 of the        *
+ *   License, or (at your option) any later version.                           *
+ *                                                                             *
+ *   This program is distributed in the hope that it will be useful,           *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *   GNU General Public License for more details.                              *
+ *                                                                             *
+ *   You should have received a copy of the GNU Library General Public         * 
+ *   License along with this program; if not, write to the                     *
+ *   Free Software Foundation, Inc.,                                           *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.                 *
+ *                                                                             *
+ *   For details about the authors of this software, see the AUTHORS file.     *
+ *******************************************************************************/
+
 package joliex.queryengine.common;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Optional;
 import jolie.Jolie;
 
 /**
  * This class implements {@link Path}s in {@link Jolie}. We use {@link Path}s in
  * the TQuery framework for ephemeral data handling over {@link Jolie} trees.
  *
- * @author Stefano Pio Zingaro <stefano.zingaro@icloud.com>
  */
 public class Path {
 
-	private static final String REGEX_STRNG = "\\.";
+	private static final String PATH_SEPARATOR = ".";
 
 	private final String node;
-	private final Path continuation;
+	private final Optional<Path> continuation;
 
-	private Path(String node, Path continuation) {
+	private Path( String node, Optional<Path> continuation ) {
 		this.node = node;
 		this.continuation = continuation;
 	}
 
 	/**
 	 * It parses a
-	 * <pre>String</pre>, e.g. <emph>a.b.c</emph>, and recursively build a
+	 * <pre>String</pre>, e.g. a.b.c, and recursively build a
 	 * {@link Path} with a node in the root and a {@link Path} as its
 	 * continuation
 	 *
 	 * @param path the <pre>String</pre> representing the {@link Path}
+	 * @return 
 	 *
-	 * @return
 	 */
-	public static Path parse(String path) {
-		return unfold(new ArrayList<>(Arrays.asList(path.split(REGEX_STRNG))));
-	}
-
-	private static Path unfold(List<String> nodes) {
-		return nodes.isEmpty() ? null : new Path(nodes.remove(0), unfold(nodes));
+	public static Path parsePath( String path ) {
+		int idx = path.indexOf( PATH_SEPARATOR );
+		if(  idx > 0 ){
+			return new Path( path.substring( 0, idx ), Optional.of( parsePath( path.substring( idx + 1 ) ) ) );
+		} else {
+			return new Path( path, Optional.empty() );
+		}
 	}
 
 	/**
-	 * It gives a nice
-	 * <pre>String</pre> representation of a {@link Path}
+	 * It gives a nice <pre>String</pre> representation of a {@link Path}
 	 *
 	 * @return the <pre>String</pre> representation of this {@link Path}
 	 */
 	public String toPrettyString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append(this.node);
-		if (this.continuation != null) {
-			sb.append(" - ");
-			sb.append(this.continuation.toPrettyString());
-		}
-		return sb.toString();
+		return node + ( continuation.isPresent() ? PATH_SEPARATOR + continuation.get().toPrettyString() : "" );	
 	}
+	
 }
--- javaServices/tqueryJavaServices/src/joliex/queryengine/match/MatchQuery.java
+++ javaServices/tqueryJavaServices/src/joliex/queryengine/match/MatchQuery.java
@@ -1,33 +1,62 @@
-/*
- *   Copyright (C) 2018 by Stefano Pio Zingaro <stefanopio.zingaro@unibo.it>
- *   Copyright (C) 2018 by Saverio Giallorenzo <saverio.giallorenzo@gmail.com>
- *
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU Library General Public License as
- *   published by the Free Software Foundation; either version 2 of the
- *   License, or (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU Library General Public
- *   License along with this program; if not, write to the
- *   Free Software Foundation, Inc.,
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- *   For details about the authors of this software, see the AUTHORS file.
- */
+/*******************************************************************************
+ *   Copyright (C) 2018 by Larisa Safina <safina@imada.sdu.dk>                 *
+ *   Copyright (C) 2018 by Stefano Pio Zingaro <stefanopio.zingaro@unibo.it>   *
+ *   Copyright (C) 2018 by Saverio Giallorenzo <saverio.giallorenzo@gmail.com> *
+ *                                                                             *
+ *   This program is free software; you can redistribute it and/or modify      *
+ *   it under the terms of the GNU Library General Public License as           *
+ *   published by the Free Software Foundation; either version 2 of the        *
+ *   License, or (at your option) any later version.                           *
+ *                                                                             *
+ *   This program is distributed in the hope that it will be useful,           *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *   GNU General Public License for more details.                              *
+ *                                                                             *
+ *   You should have received a copy of the GNU Library General Public         * 
+ *   License along with this program; if not, write to the                     *
+ *   Free Software Foundation, Inc.,                                           *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.                 *
+ *                                                                             *
+ *   For details about the authors of this software, see the AUTHORS file.     *
+ *******************************************************************************/
+
 package joliex.queryengine.match;
 
 import java.util.HashMap;
-import java.util.Optional;
 import jolie.runtime.Value;
 import jolie.runtime.ValueVector;
-import static joliex.queryengine.common.Utils.Constants.*;
 
 public final class MatchQuery {
+	
+	public static class RequestType {
+		
+		public static final String DATA = "data";
+		public static final String QUERY = "query";
+		public static class MatchExpressionType {
+			public static final String NOT = "not";
+			public static final String EQUAL = "equal";
+			public static final String GREATERTHAN = "greaterThan";
+			public static final String LOWERTHAN = "lowerThan";
+			public static final String OR = "or";
+			public static final String AND = "and";
+			public static final String EXISTS = "exists";
+			public static String[] subNodes = { NOT, EQUAL, GREATERTHAN, LOWERTHAN, OR, AND, EXISTS };
+		}
+		
+		//type MatchRequestType : void {
+		//.data*                : undefined
+		//.query                : void {
+		//    .not                  : MatchExp
+		//    | .equal              : void { .path: Path, .value[1,*]: undefined }
+		//    | .greaterThen        : void { .path: Path, .value[1,*]: undefined }
+		//    | .lowerThen          : void { .path: Path, .value[1,*]: undefined }
+		//    | .or                 : void { .left: MatchExp, .right: MatchExp }
+		//    | .and                : void { .left: MatchExp, .right: MatchExp }
+		//    | .exists             : Path
+		//    | bool
+		
+	}
 
 	private static BinaryExpression createBinaryExpression(ValueVector children) {
 		HashMap<String, ValueVector> leftRight = new HashMap<>();
@@ -44,8 +73,8 @@ public final class MatchQuery {
 				});
 		assert (leftRight.containsKey(left) && leftRight.containsKey(right));
 
-		return new BinaryExpression(createMatchExpression(leftRight.get(left).first()),
-				createMatchExpression(leftRight.get(right).first()));
+		return new BinaryExpression(parseMatchExpression(leftRight.get(left).first()),
+			parseMatchExpression(leftRight.get(right).first()));
 	}
 
 	private static CompareExp createCompareExpression(ValueVector children) {
@@ -67,12 +96,27 @@ public final class MatchQuery {
 		return new CompareExp(pathValue.get(path).first().strValue(), pathValue.get(val).first().strValue());
 	}
 
-	public static MatchExpression createMatchExpression(Value query) {
-		try {
-			if (query.hasChildren(and)) {
-				ValueVector children = query.getChildren(and);
+	public static MatchExpression parseMatchExpression( Value query ) {
+		Runnable a = new Runnable() {
+			String s;
+			Value q;
+			@Override
+			public void run() {
+				createBinaryExpression( q.getChildren( "ciao" ) );
+			}
+		};
+		
+		Runnable b = ( String s, Value q ) -> createBinaryExpression( q.getChildren( s ) );
+		HashMap< String, Runnable > hashMap = new HashMap();
+		hashMap.put( RequestType.MatchExpressionType.AND, ( q ) -> new AndExpression( createBinaryExpression( q.getChildren( RequestType.MatchExpressionType.AND ) ) ) );
+		hashMap.put( RequestType.MatchExpressionType.OR, () -> new )
 
-				return new AndExp(createBinaryExpression(children));
+		
+		try {
+				if( query.hasChildren( RequestType.MatchExpressionType.AND ) ) {
+				return new AndExpression( createBinaryExpression( query.getChildren( RequestType.MatchExpressionType.AND ) ) );
+			}
+			
 			}
 
 			if (query.hasChildren(or)) {
@@ -86,7 +130,7 @@ public final class MatchQuery {
 
 				assert (children.size() == 1);
 
-				return new NotExp(createMatchExpression(children.first()));
+				return new NotExp(parseMatchExpression(children.first()));
 			}
 
 			if (query.hasChildren(equal)) {
@@ -125,14 +169,13 @@ public final class MatchQuery {
 		throw new IllegalArgumentException("Unsupported structure of a match request");
 	}
 
-	public static Optional<Value> match(Value matchRequest) {
-		Value query = matchRequest.getChildren("query").first();
-		Value data = matchRequest.getChildren("data").first();
-		MatchExpression matchExpression = createMatchExpression(query);
+	public static Optional<Value> match( Value matchRequest ) {
 
-		return matchExpression.interpret(data);
+		Value query = matchRequest.getChildren( RequestType.QUERY ).first();
+		ValueVector data = matchRequest.getChildren( RequestType.DATA );
+
+		MatchExpression matchExpression = parseMatchExpression( query );
+
+		return matchExpression.applyOn( data );
 	}
 }
-
-
-//~ Formatted by Jindent --- http://www.jindent.com
