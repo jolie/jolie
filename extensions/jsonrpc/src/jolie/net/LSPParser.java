83fc99dc737ecb7092ebaec80785f7fed107f635
Fabrizio Montesi <famontesi@gmail.com>
2019-05-22 13:58:57 +0200

    Eferos93 jsonrpc lsp (#186)
    * # Added two classes: LSPParser and LSPMessage that that are used in
    
    * Smaller fixes
    
    Added two classes: LSPMessage and LSPParser
    Modified class JsonRpcrotocol in order to use whether HttpParser or LSPParser
    
    * Final commit (needs refactor though)
    
    * Final committ, now for real (still need refactor though)
    
    There is a lot of code repetition in the JsonRpcProtocol class, I would suggest to use inheritance and polymorphism
    
    * Refactor done + fixes
    
    -Added a method createComMessage in the JsonRpcProtocol class to get rid of repeated code.
    -in the send_internal method, in the message.isFault if branch, fixed the bug that was putting the id of the message inside of the error json object. For instance instead of {jsonrpc:"2.0", error:{... id:..}}, now it will be {jsonrpc:"2.0", id:.., error:{....}} as stated by the JSONRPC specification
    
    * Refactor done + fixes + binaries deleted
    
    -Added a method createCommMessage in the JsonRpcProtocol class to get rid of repeated code.
    -in the send_internal method, in the message.isFault if branch, fixed the bug that was putting the id of the message inside of the error json object. For instance instead of {jsonrpc:"2.0", error:{... id:..}}, now it will be {jsonrpc:"2.0", id:.., error:{....}} as stated by the JSONRPC specification
    Latest commit 01448c9  5 minutes ago
    
    * Adapted to 1.8 version
    
    * Update JsonRpcProtocol.java
    
    * JSONrpc fix

---
+++ extensions/jsonrpc/src/jolie/net/LSPParser.java
@@ -0,0 +1,154 @@
+/***************************************************************************
+ *   Copyright (C) 2019 by Fabrizio Montesi <famontesi@gmail.com>          *
+ *   Copyright (C) 2019 by Eros Fabrici <eros.fabrici@gmail.com>           *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                         *
+ *   For details about the authors of this software, see the AUTHORS file. *
+ ***************************************************************************/
+
+package jolie.net;
+
+import jolie.net.http.HttpScanner;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import jolie.lang.parse.Scanner;
+import java.net.URI;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author eferos93
+ */
+public class LSPParser {
+    private final HttpScanner scanner;
+    private Scanner.Token token;
+    
+    public LSPParser( InputStream iStream ) throws IOException {
+        scanner = new HttpScanner( iStream, URI.create( "urn:network" ) );
+    }
+    
+    private void getToken() throws IOException {
+        token = scanner.getToken();
+    }
+    
+    private void throwException() throws IOException {
+        throw new IOException("Malformed LSP header");
+    }
+    
+    private LSPMessage parseMessage() throws IOException {
+        return new LSPMessage();
+    }
+    
+    private void parseHeaderProperties( LSPMessage message ) throws IOException {
+        String name, value;
+        getToken();
+        name = token.content().toLowerCase();
+        value = scanner.readLine();
+        message.setProperty(name, value);
+        
+    }
+    
+    private static void blockingRead( InputStream stream, byte[] buffer, int offset, int length )
+		throws IOException
+    {
+		int s = 0;
+		do {
+			int r = stream.read( buffer, offset+s, length-s );
+			if ( r == -1 ) {
+				throw new EOFException();
+			}
+			s += r;
+		} while ( s < length );
+    }
+    
+    private void readContent( LSPMessage message ) throws IOException {
+        boolean chunked = false;
+		int contentLength = -1;
+
+		String p = message.getProperty( "transfer-encoding" );
+
+		if ( p != null && p.startsWith( "chunked" ) ) {
+			// Transfer-encoding has the precedence over Content-Length
+			chunked = true;
+		} else {
+		p = message.getProperty( "content-length" );
+			if ( p != null && !p.isEmpty() ) {
+				try {
+					contentLength = Integer.parseInt( p );
+					if ( contentLength == 0 ) {
+						message.setContent( new byte[0] );
+						return;
+					}
+				} catch( NumberFormatException e ) {
+					throw new IOException( "Illegal Content-Length value " + p );
+				}
+			}
+                }
+        byte[] buffer = null;
+        InputStream stream = scanner.inputStream();
+        if( chunked ) {
+            // Link: http://tools.ietf.org/html/rfc2616#section-3.6.1
+			List< byte[] > chunks = new ArrayList<> ();
+			int l = -1, totalLen = 0;
+			scanner.readChar();
+			do {
+				// the chunk header contains the size in hex format
+				// and could contain additional parameters which we ignore atm
+				String chunkHeader = scanner.readLine( false );
+				String chunkSize = chunkHeader.split( ";", 2 )[0];
+				try {
+					l = Integer.parseInt( chunkSize, 16 );
+				} catch ( NumberFormatException e ) {
+					throw new IOException( "Illegal chunk size " + chunkSize );
+				}
+				// parses the real chunk with the specified size, follwed by CR-LF
+				if ( l > 0 ) {
+					totalLen += l;
+					byte[] chunk = new byte[ l ];
+					blockingRead( stream, chunk, 0, l );
+					chunks.add( chunk );
+					scanner.readChar();
+					scanner.eatSeparators();
+				}
+			} while ( l > 0 );
+			// parse optional trailer (additional HTTP headers)
+			parseHeaderProperties( message );
+			ByteBuffer b = ByteBuffer.allocate( totalLen );
+			chunks.forEach( b::put );
+			buffer = b.array();
+        } else if (contentLength > 0) {
+            buffer = new byte[ contentLength ];
+            stream.skip(2);
+            blockingRead( stream, buffer, 0, contentLength );
+        }
+        
+        if( buffer != null ) {
+            message.setContent( buffer );
+        }
+    }
+    
+    public LSPMessage parse() throws IOException {
+        LSPMessage message = parseMessage();
+        parseHeaderProperties( message );
+        readContent( message );
+        scanner.eatSeparatorsUntilEOF();
+        return message;
+    }
+}
