789e3009734d09d37621770beea848d425d05a9a
fmontesi <famontesi@gmail.com>
2019-09-09 10:44:45 +0200

    Dev setup script for maven
    Former-commit-id: 4e0fe5190d76be2711c91a44e4873b79ba734f8f

--- scripts/dev-setup.sh
+++ scripts/dev-setup.sh
@@ -1,27 +1,8 @@
-#!/bin/zsh
-
-setopt extendedglob
+#!/bin/sh
 
 BIN_DIR=`realpath $1`
-ln -sf $(pwd)/dist/launchers/unix/* $BIN_DIR/
+ln -sf $PWD/dist/launchers/unix/* $BIN_DIR/
 
 DIST_DIR=$1/jolie-dist
 
-mkdir -p $DIST_DIR
-ln -sf $(pwd)/jolie/dist/*.jar $DIST_DIR/
-ln -sf $(pwd)/libjolie/dist/*.jar $DIST_DIR/
-
-mkdir -p $DIST_DIR/extensions
-ln -sf $(pwd)/extensions/*/dist/*.jar $DIST_DIR/extensions/
-
-mkdir -p $DIST_DIR/lib
-ln -sf $(pwd)/lib/*/dist/*.jar $DIST_DIR/lib/
-ln -sf $(pwd)/lib/^jaxws*/*.jar $DIST_DIR/lib/
-mkdir -p $DIST_DIR/lib/jaxws
-ln -sf $(pwd)/lib/jaxws/*.jar $DIST_DIR/lib/jaxws/
-ln -sf $(pwd)/support/*/dist/*.jar $DIST_DIR/lib/
-
-mkdir -p $DIST_DIR/javaServices
-ln -sf $(pwd)/javaServices/*/dist/*.jar $DIST_DIR/javaServices/
-
-ln -sf $(pwd)/include $DIST_DIR/include
+ln -sf $PWD/dist/jolie $DIST_DIR
