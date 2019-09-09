97c34839468a00524beb14df3688dd824517b8b3
fmontesi <famontesi@gmail.com>
2019-09-09 09:40:23 +0200

    Switch version read to properties file

--- libjolie/src/main/java/jolie/lang/Constants.java
+++ libjolie/src/main/java/jolie/lang/Constants.java
@@ -1,27 +1,29 @@
-/***************************************************************************
- *   Copyright (C) 2008-2019 by Fabrizio Montesi <famontesi@gmail.com>     *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU Library General Public License as       *
- *   published by the Free Software Foundation; either version 2 of the    *
- *   License, or (at your option) any later version.                       *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU Library General Public     *
- *   License along with this program; if not, write to the                 *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
- *                                                                         *
- *   For details about the authors of this software, see the AUTHORS file. *
- ***************************************************************************/
+/*
+ * Copyright (C) 2008-2019 Fabrizio Montesi <famontesi@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
 
 package jolie.lang;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.jar.Attributes;
+
 import jolie.lang.parse.Scanner;
 import jolie.util.Range;
 
@@ -33,9 +35,27 @@ import jolie.util.Range;
 public final class Constants
 {
 	// Release information
-	public static final String VERSION = "1.9.0-git";
+	public static final String VERSION = readVersionFromProperties();
 	public static final String COPYRIGHT = "(C) 2006-2019 the Jolie team";
-	
+
+	private static String readVersionFromProperties()
+	{
+		try( InputStream is = Constants.class.getClassLoader().getResourceAsStream( "libjolie.properties" ) ) {
+			if ( is == null ) {
+				return "ERROR: could not find libjolie.properties. Your distribution of Jolie might be corrupted.";
+			}
+			Properties prop = new Properties();
+			try {
+				prop.load( is );
+			} catch( IOException e ) {
+				return "ERROR: could not read libjolie.properties. Your distribution of Jolie might be corrupted.";
+			}
+			return prop.getProperty( "jolie.version" );
+		} catch( IOException e ) {
+			return "ERROR: could not close libjolie.properties correctly.";
+		}
+	}
+
 	public interface Manifest
 	{
 		// Jolie Extensions
