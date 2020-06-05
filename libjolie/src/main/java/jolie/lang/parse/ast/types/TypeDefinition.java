c7c60fdcc636c9dfaa806d69e5c03d18869128d1
Fabrizio Montesi <famontesi@gmail.com>
2020-06-05 11:20:01 +0200

    Copyright headers
    Former-commit-id: 54d3c5c3cc497419c4dc498b49f7915ff6577ddb

--- libjolie/src/main/java/jolie/lang/parse/ast/types/TypeDefinition.java
+++ libjolie/src/main/java/jolie/lang/parse/ast/types/TypeDefinition.java
@@ -1,25 +1,23 @@
-/***************************************************************************
- *   Copyright (C) 2008 by Elvis Ciotti                                    *
- *   Copyright (C) 2009-2015 by Fabrizio Montesi <famontesi@gmail.com>     *
- *   Copyright (C) 2011 by Claudio Guidi                                   *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU Library General Public License as       *
- *   published by the Free Software Foundation; either version 2 of the    *
- *   License, or (at your option) any later version.                       *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU Library General Public     *
- *   License along with this program; if not, write to the                 *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
- *                                                                         *
- *   For details about the authors of this software, see the AUTHORS file. *
- ***************************************************************************/
+/*
+ * Copyright (C) 2008 Elvis Ciotti
+ * Copyright (C) 2008-2020 Fabrizio Montesi <famontesi@gmail.com>
+ * Copyright (C) 2011 Claudio Guidi
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
 
 package jolie.lang.parse.ast.types;
 
