22b032d86337adafe96baf834a6748e19983d5e7
Fabrizio Montesi <famontesi@gmail.com>
2021-10-26 19:12:32 +0200

    Update FileService.java

--- javaServices/coreJavaServices/src/main/java/joliex/io/FileService.java
+++ javaServices/coreJavaServices/src/main/java/joliex/io/FileService.java
@@ -759,7 +759,7 @@ public class FileService extends JavaService {
 				Value fileValue = Value.create( p.toString() );
 				if( fileInfo ) {
 					Value info = fileValue.getFirstChild( "info" );
-					File currFile = new File( path.toString() );
+					File currFile = path.toFile();
 					info.getFirstChild( "lastModified" ).setValue( currFile.lastModified() );
 					info.getFirstChild( "size" ).setValue( currFile.length() );
 					info.getFirstChild( "absolutePath" ).setValue( currFile.getAbsolutePath() );
