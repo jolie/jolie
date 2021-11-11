e0df87a4d927cdde00bfc346923f9703107492e0
vicki <33627612+vickimixen@users.noreply.github.com>
2021-11-11 08:35:42 +0100

    Prints all files and directories found, from the paths where it could not find the module it was trying to import

--- libjolie/src/main/java/jolie/lang/parse/module/exceptions/ModuleNotFoundException.java
+++ libjolie/src/main/java/jolie/lang/parse/module/exceptions/ModuleNotFoundException.java
@@ -20,9 +20,17 @@
 package jolie.lang.parse.module.exceptions;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import jolie.lang.Constants;
 
 public class ModuleNotFoundException extends FileNotFoundException {
@@ -46,8 +54,83 @@ public class ModuleNotFoundException extends FileNotFoundException {
 
 	@Override
 	public String getMessage() {
-		return "Module \"" + this.moduleName + "\" not found from lookup path "
-			+ Arrays.toString( this.lookedPaths.toArray() );
+		StringBuilder message =
+			new StringBuilder( "Module \"" ).append( this.moduleName )
+				.append( "\" not found from lookup paths: (this line is printed twice for some reason)\n" );
+		Set< String > fileNames = new HashSet<>();
+		String[] moduleNameParts = (this.moduleName.substring( 1, this.moduleName.length() - 1 )).split( ", " );
+		for( String ms : this.lookedPaths ) {
+			message.append( ms ).append( "\n" );
+			try {
+				if( ms.startsWith( moduleNameParts[ 0 ] ) ) {
+					Stream< Path > stream;
+					stream = Files.list( Paths.get( ".\\" ) );
+					fileNames
+						.addAll( stream.map( Path::getFileName ).map( Path::toString ).collect( Collectors.toSet() ) );
+					stream.close();
+					String trailing = "";
+					for( String parts : moduleNameParts ) {
+						Stream< Path > forloopStream;
+						if( !moduleNameParts[ moduleNameParts.length - 1 ].equals( parts ) ) {
+							trailing += parts + "\\";
+							forloopStream = Files.list( Paths.get( ".\\" + trailing ) );
+							fileNames.addAll( forloopStream.map( Path::getFileName ).map( Path::toString )
+								.collect( Collectors.toSet() ) );
+							forloopStream.close();
+						}
+
+					}
+				} else if( ms.startsWith( ".\\" ) ) {
+					String trailing = "";
+					String[] pathParts = ms.split( "\\\\" );
+					String lastPart = pathParts[ pathParts.length - 1 ];
+					for( String parts : pathParts ) {
+						Stream< Path > forloopStream;
+						if( !lastPart.equals( parts ) ) {
+							trailing += parts + "\\";
+							forloopStream = Files.list( Paths.get( trailing ) );
+							fileNames.addAll( forloopStream.map( Path::getFileName ).map( Path::toString )
+								.collect( Collectors.toSet() ) );
+							forloopStream.close();
+						}
+
+					}
+				} else if( ms.startsWith( System.getenv( "JOLIE_HOME" ) ) ) {
+					Stream< Path > stream;
+					String path = System.getenv( "JOLIE_HOME" ) + "\\packages";
+					stream = Files.list( Paths.get( path ) );
+					fileNames
+						.addAll( stream.map( Path::getFileName ).map( Path::toString ).collect( Collectors.toSet() ) );
+					stream.close();
+					String trailing = path + "\\";
+					for( String parts : moduleNameParts ) {
+						Stream< Path > forloopStream;
+						if( !moduleNameParts[ moduleNameParts.length - 1 ].equals( parts ) ) {
+							trailing += parts + "\\";
+							forloopStream = Files.list( Paths.get( trailing ) );
+							fileNames.addAll( forloopStream.map( Path::getFileName ).map( Path::toString )
+								.collect( Collectors.toSet() ) );
+							forloopStream.close();
+						}
+
+					}
+				} else if( ms.startsWith( System.getProperty( "user.dir" ) + "\\lib" ) ) {
+					Stream< Path > stream;
+					int index = ms.indexOf( moduleNameParts[ 0 ] + ".jap" );
+					String path = ms.substring( 0, index );
+					stream = Files.list( Paths.get( path ) );
+					fileNames
+						.addAll( stream.map( Path::getFileName ).map( Path::toString ).collect( Collectors.toSet() ) );
+					stream.close();
+				}
+			} catch( IOException e ) {
+			}
+		}
+		message.append( "maybe you meant: (this is also printed twice for some reason)\n" );
+		for( String p : fileNames ) {
+			message.append( p ).append( "\n" );
+		}
+		return message.toString();
 	}
 
 }
