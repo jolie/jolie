ab744b91f1e743fb0b5460eaf65d36e71cc0a224
vicki <33627612+vickimixen@users.noreply.github.com>
2021-11-24 10:25:11 +0100

    Fixed the module error message without using parsingcontext, as this was not currently possible

--- libjolie/src/main/java/jolie/lang/parse/module/exceptions/ModuleNotFoundException.java
+++ libjolie/src/main/java/jolie/lang/parse/module/exceptions/ModuleNotFoundException.java
@@ -20,17 +20,9 @@
 package jolie.lang.parse.module.exceptions;
 
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-import java.util.HashSet;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import org.apache.commons.text.similarity.LevenshteinDistance;
 
 import jolie.lang.Constants;
 import jolie.lang.parse.module.ImportPath;
@@ -54,68 +46,12 @@ public class ModuleNotFoundException extends FileNotFoundException {
 		this.lookedPaths.add( lookedPath );
 	}
 
-	public String getHelp() {
-		StringBuilder message = new StringBuilder();
-		Set< String > fileNames = new HashSet<>();
-		Stream< Path > stream;
-		try {
-			stream = Files.list( Paths.get( ".\\" ) );
-			fileNames.addAll( stream.filter( file -> !Files.isDirectory( file ) ).map( Path::getFileName )
-				.map( Path::toString ).collect( Collectors.toSet() ) );
-			stream.close();
-		} catch( IOException e ) {
-		}
-		for( Path path : this.lookedPaths ) {
-			Stream< Path > forloopStream;
-			try {
-				Path currentPath = path;
-				while( !Files.isDirectory( currentPath ) ) {
-					currentPath = currentPath.getParent();
-				}
-				forloopStream = Files.list( currentPath );
-				fileNames.addAll( forloopStream.filter( file -> !Files.isDirectory( file ) ).map( Path::getFileName )
-					.map( Path::toString ).collect( Collectors.toSet() ) );
-				forloopStream.close();
-			} catch( IOException e ) {
-			}
-		}
+	public final List< Path > lookedPaths() {
+		return this.lookedPaths;
+	}
 
-		LevenshteinDistance dist = new LevenshteinDistance();
-		ArrayList< String > proposedModules = new ArrayList<>();
-		for( String correctModule : fileNames ) {
-			String moduleName;
-			if( correctModule.contains( "." ) ) {
-				int column = correctModule.indexOf( "." );
-				moduleName = correctModule.substring( 0, column );
-			} else {
-				moduleName = correctModule;
-			}
-			if( dist.apply( this.importPath.toString(), moduleName ) <= 2 ) {
-				proposedModules.add( moduleName );
-			}
-
-		}
-		if( !proposedModules.isEmpty() ) {
-			message.append( "Maybe you meant:\n" );
-			for( String module : proposedModules ) {
-				String temp = module.substring( 0, 1 ).toUpperCase() + module.substring( 1 );
-				message.append( temp ).append( "\n" );
-			}
-		} else {
-			message.append( "Could not find modules matching \"" ).append( this.importPath )
-				.append( "\". Here are some modules that can be imported:\n" );
-			for( String module : fileNames ) {
-				String temp;
-				if( module.contains( "." ) ) {
-					int column = module.indexOf( "." );
-					temp = module.substring( 0, 1 ).toUpperCase() + module.substring( 1, column );
-				} else {
-					temp = module.substring( 0, 1 ).toUpperCase() + module.substring( 1 );
-				}
-				message.append( temp ).append( "\n" );
-			}
-		}
-		return message.toString();
+	public final ImportPath importPath() {
+		return this.importPath;
 	}
 
 	@Override
@@ -125,5 +61,4 @@ public class ModuleNotFoundException extends FileNotFoundException {
 				.append( "\" not found from lookup paths.\n" );
 		return message.toString();
 	}
-
 }
