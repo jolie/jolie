07443a0640cae43880b0f79c957fe9ecc23ca3ae
Fabrizio Montesi <famontesi@gmail.com>
2021-12-07 20:36:24 +0100

    support for inline instanceof types

--- libjolie/src/main/java/jolie/lang/parse/OLParser.java
+++ libjolie/src/main/java/jolie/lang/parse/OLParser.java
@@ -3116,21 +3116,21 @@ public class OLParser extends AbstractParser {
 		return andCond;
 	}
 
-	private NativeType readNativeType() {
-		if( token.is( Scanner.TokenType.CAST_INT ) ) {
-			return NativeType.INT;
-		} else if( token.is( Scanner.TokenType.CAST_DOUBLE ) ) {
-			return NativeType.DOUBLE;
-		} else if( token.is( Scanner.TokenType.CAST_STRING ) ) {
-			return NativeType.STRING;
-		} else if( token.is( Scanner.TokenType.CAST_LONG ) ) {
-			return NativeType.LONG;
-		} else if( token.is( Scanner.TokenType.CAST_BOOL ) ) {
-			return NativeType.BOOL;
-		} else {
-			return NativeType.fromString( token.content() );
-		}
-	}
+	// private NativeType readNativeType() {
+	// if( token.is( Scanner.TokenType.CAST_INT ) ) {
+	// return NativeType.INT;
+	// } else if( token.is( Scanner.TokenType.CAST_DOUBLE ) ) {
+	// return NativeType.DOUBLE;
+	// } else if( token.is( Scanner.TokenType.CAST_STRING ) ) {
+	// return NativeType.STRING;
+	// } else if( token.is( Scanner.TokenType.CAST_LONG ) ) {
+	// return NativeType.LONG;
+	// } else if( token.is( Scanner.TokenType.CAST_BOOL ) ) {
+	// return NativeType.BOOL;
+	// } else {
+	// return NativeType.fromString( token.content() );
+	// }
+	// }
 
 	private OLSyntaxNode parseBasicCondition()
 		throws IOException, ParserException {
@@ -3152,17 +3152,19 @@ public class OLParser extends AbstractParser {
 		} else if( opType == Scanner.TokenType.INSTANCE_OF ) {
 			nextToken();
 
-			NativeType nativeType = readNativeType();
-			if( nativeType == null ) { // It's a user-defined type
-				assertToken( Scanner.TokenType.ID, "expected type name after instanceof" );
-			}
+			// NativeType nativeType = readNativeType();
+			// if( nativeType == null ) { // It's a user-defined type
+			// assertToken( Scanner.TokenType.ID, "expected type name after instanceof" );
+			// }
 
-			String typeName = token.content();
-			TypeDefinition type = definedTypes.getOrDefault( typeName, new TypeDefinitionLink( getContext(),
-				typeName, Constants.RANGE_ONE_TO_ONE, typeName ) );
+			// String typeName = token.content();
+			// TypeDefinition type = definedTypes.getOrDefault( typeName, new TypeDefinitionLink( getContext(),
+			// typeName, Constants.RANGE_ONE_TO_ONE, typeName ) );
 
-			ret = new InstanceOfExpressionNode( getContext(), expr1, type );
-			nextToken();
+			// TODO: parseType should be refactored to return unnamed TypeExpressions
+			ret =
+				new InstanceOfExpressionNode( getContext(), expr1, parseType( "#InlineType", AccessModifier.PRIVATE ) );
+			// nextToken();
 		} else {
 			ret = expr1;
 		}
