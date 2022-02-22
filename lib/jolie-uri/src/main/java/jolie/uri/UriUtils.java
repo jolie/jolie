29245cd700e6aea5b1e6804e255adba1d34ff13b
Balint Maschio <bmaschio77@gmail.com>
2022-02-22 13:47:33 +0100

    removed @AndJarDeps

--- lib/jolie-uri/src/main/java/jolie/uri/UriUtils.java
+++ lib/jolie-uri/src/main/java/jolie/uri/UriUtils.java
@@ -28,7 +28,7 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-@AndJarDeps( { "uri-templates.jar", "joda-time.jar" } )
+
 public class UriUtils {
 	public static Value match( Value request ) {
 		return match( request.getFirstChild( "template" ).strValue(), request.getFirstChild( "uri" ).strValue() );
