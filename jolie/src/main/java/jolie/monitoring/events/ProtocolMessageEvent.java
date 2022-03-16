0373157a0d1e22d27ecdcd0d269a15ae80f1e18e
Mauro <msgarzi@imolainformatica.it>
2022-03-16 08:27:53 +0100

    aggiunto ad eventi ProtocolMessageEvent.java - wip

---
+++ jolie/src/main/java/jolie/monitoring/events/ProtocolMessageEvent.java
@@ -0,0 +1,59 @@
+package jolie.monitoring.events;
+
+import jolie.lang.parse.context.ParsingContext;
+import jolie.monitoring.MonitoringEvent;
+import jolie.runtime.Value;
+
+public class ProtocolMessageEvent extends MonitoringEvent {
+ 
+	public enum Protocol {
+		SOAP( "soap" ), HTTP( "http" );
+
+		private String protocol;
+
+		Protocol( String protocol ) {
+			this.protocol = protocol;
+		}
+
+		public String getProtocol() {
+			return protocol;
+		}
+	}
+
+	public static enum FieldNames {
+		PROTOCOL( "protocol" ), MESSAGE( "message" ), HEADER( "header" );
+
+		private String fieldName;
+
+		FieldNames( String name ) {
+			this.fieldName = name;
+		}
+
+		public String getName() {
+			return this.fieldName;
+		}
+	}
+
+	public static String protocol( Value value ) {
+		return value.getFirstChild( FieldNames.PROTOCOL.getName() ).strValue();
+	}
+
+	public static String message( Value value ) {
+		return value.getFirstChild( FieldNames.MESSAGE.getName() ).strValue();
+	}
+
+	public static String header( Value value ) {
+		return value.getFirstChild( FieldNames.HEADER.getName() ).strValue();
+	}
+
+
+	public ProtocolMessageEvent( String message, String header, String service,
+		ProtocolMessageEvent.Protocol protocol, String processId, String scope, ParsingContext context ) {
+
+		super( EventTypes.PROTOCOL_MESSAGE, service, scope, processId, context, Value.create() );
+
+		data().getFirstChild( FieldNames.PROTOCOL.getName() ).setValue( protocol.getProtocol() );
+		data().getFirstChild( FieldNames.HEADER.getName() ).setValue( header );
+		data().getFirstChild( FieldNames.MESSAGE.getName() ).setValue( message );
+	}
+}
