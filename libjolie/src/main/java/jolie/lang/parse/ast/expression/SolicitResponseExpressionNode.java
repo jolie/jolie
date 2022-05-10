1963b3c6fb025f7fd23f29b7252c5d6b295c2013
Hhssman <stevenhhssman@gmail.com>
2022-05-10 14:19:42 +0200

    Removed commented out code from SolicitResponseExpressionNode

--- libjolie/src/main/java/jolie/lang/parse/ast/expression/SolicitResponseExpressionNode.java
+++ libjolie/src/main/java/jolie/lang/parse/ast/expression/SolicitResponseExpressionNode.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2022 Steven Ensted <stevenhhssman@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+
 package jolie.lang.parse.ast.expression;
 
 import jolie.lang.parse.OLVisitor;
@@ -7,21 +26,15 @@ import jolie.lang.parse.context.ParsingContext;
 public class SolicitResponseExpressionNode extends OLSyntaxNode {
 	private final String id, outputPortId;
 	private final OLSyntaxNode outputExpression;
-	// private final InstallFunctionNode handlersFunction;
 
-	// TODO handle if id is null
 	public SolicitResponseExpressionNode( ParsingContext context, String id, String outputPortId,
 		OLSyntaxNode outputExpression ) {
 		super( context );
 		this.id = id;
 		this.outputPortId = outputPortId;
 		this.outputExpression = outputExpression;
-		// this.handlersFunction = handlersFunction.orElse( null );
-	}
 
-	/*
-	 * public InstallFunctionNode handlerFunction() { return handlersFunction; }
-	 */
+	}
 
 	public String id() {
 		return id;
@@ -39,5 +52,4 @@ public class SolicitResponseExpressionNode extends OLSyntaxNode {
 	public < C, R > R accept( OLVisitor< C, R > visitor, C ctx ) {
 		return visitor.visit( this, ctx );
 	}
-
 }
