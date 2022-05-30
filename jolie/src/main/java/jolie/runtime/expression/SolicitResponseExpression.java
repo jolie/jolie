2cb1ebb2aae958b557a88e3b727db33135af928a
Fabrizio Montesi <famontesi@gmail.com>
2022-05-30 16:34:18 +0200

    Fixes

--- jolie/src/main/java/jolie/runtime/expression/SolicitResponseExpression.java
+++ jolie/src/main/java/jolie/runtime/expression/SolicitResponseExpression.java
@@ -47,229 +47,226 @@ import jolie.tracer.MessageTraceAction;
 import jolie.tracer.Tracer;
 
 public class SolicitResponseExpression implements Expression {
+	private final String operationId;
+	private final OutputPort outputPort;
+	private final Expression outputExpression; // may be null
+	private final RequestResponseTypeDescription types;
+	private final ParsingContext context;
+	private final VariablePath inputVarPath; // may be null
 
-    private final String operationId;
-    private final OutputPort outputPort;
-    private final Expression outputExpression; // may be null
-    private final RequestResponseTypeDescription types;
-    private final ParsingContext context;
-    private final VariablePath inputVarPath; // may be null
+	public SolicitResponseExpression(
+		String operationId,
+		OutputPort outputPort,
+		Expression outputExpression,
+		RequestResponseTypeDescription types,
+		ParsingContext context ) {
+		this.operationId = operationId;
+		this.outputPort = outputPort;
+		this.outputExpression = outputExpression;
+		this.types = types;
+		this.context = context;
+		this.inputVarPath = null;
+	}
 
-    public SolicitResponseExpression(
-        String operationId,
-        OutputPort outputPort,
-        Expression outputExpression,
-        RequestResponseTypeDescription types,
-        ParsingContext context ) {
-        this.operationId = operationId;
-        this.outputPort = outputPort;
-        this.outputExpression = outputExpression;
-        this.types = types;
-        this.context = context;
-        this.inputVarPath = null;
-    }
+	public SolicitResponseExpression(
+		String operationId,
+		OutputPort outputPort,
+		Expression outputExpression,
+		RequestResponseTypeDescription types,
+		ParsingContext context,
+		VariablePath inputVarPath ) {
+		this.operationId = operationId;
+		this.outputPort = outputPort;
+		this.outputExpression = outputExpression;
+		this.types = types;
+		this.context = context;
+		this.inputVarPath = inputVarPath;
+	}
 
-    public SolicitResponseExpression(
-        String operationId,
-        OutputPort outputPort,
-        Expression outputExpression,
-        RequestResponseTypeDescription types,
-        ParsingContext context,
-        VariablePath inputVarPath ) {
-        this.operationId = operationId;
-        this.outputPort = outputPort;
-        this.outputExpression = outputExpression;
-        this.types = types;
-        this.context = context;
-        this.inputVarPath = inputVarPath;
-    }
+	@Override
+	public Expression cloneExpression( TransformationReason reason ) {
+		return new SolicitResponseExpression(
+			operationId,
+			outputPort,
+			(outputExpression == null) ? null : outputExpression.cloneExpression( reason ),
+			types,
+			context );
+	}
 
-    @Override
-    public Expression cloneExpression( TransformationReason reason ) {
-        return new SolicitResponseExpression(
-            operationId,
-            outputPort,
-            (outputExpression == null) ? null : outputExpression.cloneExpression( reason ),
-            types,
-            context );
-    }
-
-    private void log( String log, CommMessage message ) {
-        final Tracer tracer = Interpreter.getInstance().tracer();
-        tracer.trace( () -> new MessageTraceAction(
-            MessageTraceAction.Type.SOLICIT_RESPONSE,
-            operationId + "@" + outputPort.id(),
-            log,
-            message,
-            context ) );
-    }
+	private void log( String log, CommMessage message ) {
+		final Tracer tracer = Interpreter.getInstance().tracer();
+		tracer.trace( () -> new MessageTraceAction(
+			MessageTraceAction.Type.SOLICIT_RESPONSE,
+			operationId + "@" + outputPort.id(),
+			log,
+			message,
+			context ) );
+	}
 
 
-    @Override
-    public Value evaluate()
-        throws FaultException.RuntimeFaultException {
-        CommChannel channel = null;
-        CommMessage response = null;
-        try {
+	@Override
+	public Value evaluate()
+		throws FaultException.RuntimeFaultException {
+		CommChannel channel = null;
+		CommMessage response = null;
+		try {
+			CommMessage message =
+				CommMessage.createRequest(
+					operationId,
+					outputPort.getResourcePath(),
+					(outputExpression == null) ? Value.UNDEFINED_VALUE : outputExpression.evaluate() );
 
-            CommMessage message =
-                CommMessage.createRequest(
-                    operationId,
-                    outputPort.getResourcePath(),
-                    (outputExpression == null) ? Value.UNDEFINED_VALUE : outputExpression.evaluate() );
-
-            log( "SENDING", message );
-            if( types.requestType() != null ) {
-                try {
-                    types.requestType().check( message.value() );
-                } catch( TypeCheckingException e ) {
-                    log( "TYPE MISMATCH", message );
-                    // just for logging also cause
-                    Value tmpValue = Value.create();
-                    tmpValue.setValue( e.getMessage() );
-                    log( "TYPE MISMATCH", new CommMessage( message.requestId(), message.operationName(),
-                        message.resourcePath(), tmpValue, null ) );
-                    if( Interpreter.getInstance().isMonitoring() ) {
-                        Interpreter.getInstance().fireMonitorEvent(
-                            new OperationCallEvent( operationId, ExecutionThread.currentThread().getSessionId(),
-                                Long.toString( message.requestId() ), OperationCallEvent.FAULT,
-                                "TypeMismatch:" + e.getMessage(), outputPort.id(), message.value(),
+			log( "SENDING", message );
+			if( types.requestType() != null ) {
+				try {
+					types.requestType().check( message.value() );
+				} catch( TypeCheckingException e ) {
+					log( "TYPE MISMATCH", message );
+					// just for logging also cause
+					Value tmpValue = Value.create();
+					tmpValue.setValue( e.getMessage() );
+					log( "TYPE MISMATCH", new CommMessage( message.requestId(), message.operationName(),
+						message.resourcePath(), tmpValue, null ) );
+					if( Interpreter.getInstance().isMonitoring() ) {
+						Interpreter.getInstance().fireMonitorEvent(
+							new OperationCallEvent( operationId, ExecutionThread.currentThread().getSessionId(),
+								Long.toString( message.requestId() ), OperationCallEvent.FAULT,
+								"TypeMismatch:" + e.getMessage(), outputPort.id(), message.value(),
 								Long.toString( message.id() ) ) );
-                    }
+					}
 
-                    throw (e);
-                }
-            }
+					throw e;
+				}
+			}
 
-            channel = outputPort.getCommChannel();
-            channel.send( message );
-            // channel.release(); TODO release channel if possible (i.e. it will not be closed)
-            log( "SENT", message );
-            if( Interpreter.getInstance().isMonitoring() ) {
-                Interpreter.getInstance()
-                    .fireMonitorEvent( new OperationCallEvent( operationId,
-                        ExecutionThread.currentThread().getSessionId(), Long.toString( message.requestId() ),
-                        OperationCallEvent.SUCCESS, "", outputPort.id(), message.value(),
-								Long.toString( message.id() ) ) );
-            }
+			channel = outputPort.getCommChannel();
+			channel.send( message );
+			// channel.release(); TODO release channel if possible (i.e. it will not be closed)
+			log( "SENT", message );
+			if( Interpreter.getInstance().isMonitoring() ) {
+				Interpreter.getInstance()
+					.fireMonitorEvent( new OperationCallEvent( operationId,
+						ExecutionThread.currentThread().getSessionId(), Long.toString( message.requestId() ),
+						OperationCallEvent.SUCCESS, "", outputPort.id(), message.value(),
+						Long.toString( message.id() ) ) );
+			}
 
-            do {
-                try {
-                    response = channel.recvResponseFor( message ).get( Interpreter.getInstance().responseTimeout(),
-                        TimeUnit.MILLISECONDS );
-                } catch( InterruptedException e ) {
-                    throw new IOException( e );
-                } catch( ExecutionException e ) {
-                    if( e.getCause() instanceof IOException ) {
-                        throw (IOException) e.getCause();
-                    } else {
-                        throw new IOException( e.getCause() );
-                    }
-                }
-            } while( response == null );
-            log( "RECEIVED", response );
+			do {
+				try {
+					response = channel.recvResponseFor( message ).get( Interpreter.getInstance().responseTimeout(),
+						TimeUnit.MILLISECONDS );
+				} catch( InterruptedException e ) {
+					throw new IOException( e );
+				} catch( ExecutionException e ) {
+					if( e.getCause() instanceof IOException ) {
+						throw (IOException) e.getCause();
+					} else {
+						throw new IOException( e.getCause() );
+					}
+				}
+			} while( response == null );
+			log( "RECEIVED", response );
 
-            if( inputVarPath != null ) {
-                inputVarPath.setValue( response.value() );
-            }
+			if( inputVarPath != null ) {
+				inputVarPath.setValue( response.value() );
+			}
 
-            if( response.isFault() ) {
-                Type faultType = types.getFaultType( response.fault().faultName() );
-                if( faultType != null ) {
-                    try {
-                        faultType.check( response.fault().value() );
-                        if( Interpreter.getInstance().isMonitoring() ) {
-                            Interpreter.getInstance()
-                                .fireMonitorEvent( new OperationReplyEvent( operationId,
-                                    ExecutionThread.currentThread().getSessionId(),
-                                    Long.toString( response.requestId() ), OperationReplyEvent.FAULT,
-                                    response.fault().faultName(), outputPort.id(), response.fault().value(),
+			if( response.isFault() ) {
+				Type faultType = types.getFaultType( response.fault().faultName() );
+				if( faultType != null ) {
+					try {
+						faultType.check( response.fault().value() );
+						if( Interpreter.getInstance().isMonitoring() ) {
+							Interpreter.getInstance()
+								.fireMonitorEvent( new OperationReplyEvent( operationId,
+									ExecutionThread.currentThread().getSessionId(),
+									Long.toString( response.requestId() ), OperationReplyEvent.FAULT,
+									response.fault().faultName(), outputPort.id(), response.fault().value(),
 									Long.toString( response.id() ) ) );
-                        }
-                    } catch( TypeCheckingException e ) {
-                        if( Interpreter.getInstance().isMonitoring() ) {
-                            Interpreter.getInstance()
-                                .fireMonitorEvent( new OperationReplyEvent( operationId,
-                                    ExecutionThread.currentThread().getSessionId(),
-                                    Long.toString( response.requestId() ), OperationReplyEvent.FAULT,
-                                    "TypeMismatch on fault:" + response.fault().faultName() + "." + e.getMessage(),
-                                    outputPort.id(), response.fault().value(),
+						}
+					} catch( TypeCheckingException e ) {
+						if( Interpreter.getInstance().isMonitoring() ) {
+							Interpreter.getInstance()
+								.fireMonitorEvent( new OperationReplyEvent( operationId,
+									ExecutionThread.currentThread().getSessionId(),
+									Long.toString( response.requestId() ), OperationReplyEvent.FAULT,
+									"TypeMismatch on fault:" + response.fault().faultName() + "." + e.getMessage(),
+									outputPort.id(), response.fault().value(),
 									Long.toString( response.id() ) ) );
-                        }
-                        throw new FaultException( Constants.TYPE_MISMATCH_FAULT_NAME,
-                            "Received fault " + response.fault().faultName() + " TypeMismatch (" + operationId + "@"
-                                + outputPort.id() + "): " + e.getMessage() ).toRuntimeFaultException();
-                    }
-                } else {
-                    if( Interpreter.getInstance().isMonitoring() ) {
-                        Interpreter.getInstance().fireMonitorEvent(
-                            new OperationReplyEvent( operationId, ExecutionThread.currentThread().getSessionId(),
-                                Long.toString( response.requestId() ), OperationReplyEvent.FAULT,
-                                response.fault().faultName(), outputPort.id(), response.fault().value(),
+						}
+						throw new FaultException( Constants.TYPE_MISMATCH_FAULT_NAME,
+							"Received fault " + response.fault().faultName() + " TypeMismatch (" + operationId + "@"
+								+ outputPort.id() + "): " + e.getMessage() ).toRuntimeFaultException();
+					}
+				} else {
+					if( Interpreter.getInstance().isMonitoring() ) {
+						Interpreter.getInstance().fireMonitorEvent(
+							new OperationReplyEvent( operationId, ExecutionThread.currentThread().getSessionId(),
+								Long.toString( response.requestId() ), OperationReplyEvent.FAULT,
+								response.fault().faultName(), outputPort.id(), response.fault().value(),
+								Long.toString( response.id() ) ) );
+					}
+				}
+				throw response.fault();
+			} else {
+				if( types.responseType() != null ) {
+					try {
+						types.responseType().check( response.value() );
+						if( Interpreter.getInstance().isMonitoring() ) {
+							Interpreter.getInstance()
+								.fireMonitorEvent( new OperationReplyEvent( operationId,
+									ExecutionThread.currentThread().getSessionId(),
+									Long.toString( response.requestId() ), OperationReplyEvent.SUCCESS, "",
+									outputPort.id(), response.value(),
 									Long.toString( response.id() ) ) );
-                    }
-                }
-                throw response.fault();
-            } else {
-                if( types.responseType() != null ) {
-                    try {
-                        types.responseType().check( response.value() );
-                        if( Interpreter.getInstance().isMonitoring() ) {
-                            Interpreter.getInstance()
-                                .fireMonitorEvent( new OperationReplyEvent( operationId,
-                                    ExecutionThread.currentThread().getSessionId(),
-                                    Long.toString( response.requestId() ), OperationReplyEvent.SUCCESS, "",
-                                    outputPort.id(), response.value(),
+						}
+					} catch( TypeCheckingException e ) {
+						if( Interpreter.getInstance().isMonitoring() ) {
+							Interpreter.getInstance()
+								.fireMonitorEvent( new OperationReplyEvent( operationId,
+									ExecutionThread.currentThread().getSessionId(),
+									Long.toString( response.requestId() ), OperationReplyEvent.FAULT, e.getMessage(),
+									outputPort.id(), response.value(),
 									Long.toString( response.id() ) ) );
-                        }
-                    } catch( TypeCheckingException e ) {
-                        if( Interpreter.getInstance().isMonitoring() ) {
-                            Interpreter.getInstance()
-                                .fireMonitorEvent( new OperationReplyEvent( operationId,
-                                    ExecutionThread.currentThread().getSessionId(),
-                                    Long.toString( response.requestId() ), OperationReplyEvent.FAULT, e.getMessage(),
-                                    outputPort.id(), response.value(),
-									Long.toString( response.id() ) ) );
-                        }
-                        throw new FaultException( Constants.TYPE_MISMATCH_FAULT_NAME, "Received message TypeMismatch ("
-                            + operationId + "@" + outputPort.id() + "): " + e.getMessage() ).toRuntimeFaultException();
-                    }
-                } else {
-                    if( Interpreter.getInstance().isMonitoring() ) {
-                        Interpreter.getInstance().fireMonitorEvent( new OperationReplyEvent( operationId,
-                            ExecutionThread.currentThread().getSessionId(), Long.toString( response.requestId() ),
-                            OperationReplyEvent.SUCCESS, "", outputPort.id(), response.value(),
-									Long.toString( response.id() ) ) );
-                    }
-                }
-            }
+						}
+						throw new FaultException( Constants.TYPE_MISMATCH_FAULT_NAME, "Received message TypeMismatch ("
+							+ operationId + "@" + outputPort.id() + "): " + e.getMessage() ).toRuntimeFaultException();
+					}
+				} else {
+					if( Interpreter.getInstance().isMonitoring() ) {
+						Interpreter.getInstance().fireMonitorEvent( new OperationReplyEvent( operationId,
+							ExecutionThread.currentThread().getSessionId(), Long.toString( response.requestId() ),
+							OperationReplyEvent.SUCCESS, "", outputPort.id(), response.value(),
+							Long.toString( response.id() ) ) );
+					}
+				}
+			}
 
-        } catch( TimeoutException e ) { // The response timed out
-            throw new FaultException( Constants.TIMEOUT_EXCEPTION_FAULT_NAME ).toRuntimeFaultException();
-        } catch( IOException e ) {
-            throw new FaultException( Constants.IO_EXCEPTION_FAULT_NAME, e ).toRuntimeFaultException();
-        } catch( URISyntaxException e ) {
-            Interpreter.getInstance().logSevere( e );
-        } catch( TypeCheckingException e ) {
-            throw new FaultException( Constants.TYPE_MISMATCH_FAULT_NAME,
-                "Output message TypeMismatch (" + operationId + "@" + outputPort.id() + "): " + e.getMessage() ).toRuntimeFaultException();
-        } catch ( FaultException e ){
-            throw e.toRuntimeFaultException();
-        } finally {
-            if( channel != null ) {
-                try {
-                    channel.release();
-                } catch( IOException e ) {
-                    Interpreter.getInstance().logWarning( e );
-                }
-            }
-        }
+		} catch( TimeoutException e ) { // The response timed out
+			throw new FaultException( Constants.TIMEOUT_EXCEPTION_FAULT_NAME ).toRuntimeFaultException();
+		} catch( IOException e ) {
+			throw new FaultException( Constants.IO_EXCEPTION_FAULT_NAME, e ).toRuntimeFaultException();
+		} catch( URISyntaxException e ) {
+			Interpreter.getInstance().logSevere( e );
+		} catch( TypeCheckingException e ) {
+			throw new FaultException( Constants.TYPE_MISMATCH_FAULT_NAME,
+				"Output message TypeMismatch (" + operationId + "@" + outputPort.id() + "): " + e.getMessage() )
+					.toRuntimeFaultException();
+		} catch( FaultException e ) {
+			throw e.toRuntimeFaultException();
+		} finally {
+			if( channel != null ) {
+				try {
+					channel.release();
+				} catch( IOException e ) {
+					Interpreter.getInstance().logWarning( e );
+				}
+			}
+		}
 
-        if(response != null){
-            return response.value();
-        }else{
-            return null;
-        }
-    }
-
+		if( response == null ) {
+			throw new AssertionError( "The value returned by a solicit-response expression should never be null" );
+		}
+		return response.value();
+	}
 }
--- libjolie/src/main/java/jolie/lang/parse/ast/expression/SolicitResponseExpressionNode.java
+++ libjolie/src/main/java/jolie/lang/parse/ast/expression/SolicitResponseExpressionNode.java
@@ -25,36 +25,36 @@ import jolie.lang.parse.ast.OLSyntaxNode;
 import jolie.lang.parse.context.ParsingContext;
 
 public class SolicitResponseExpressionNode extends OLSyntaxNode {
-    private final String id;
-    private final String outputPortId;
-    private final OLSyntaxNode outputExpression;
+	private final String id;
+	private final String outputPortId;
+	private final OLSyntaxNode outputExpression;
 
-    public SolicitResponseExpressionNode(
-        ParsingContext context,
-        String id,
-        String outputPortId,
-        OLSyntaxNode outputExpression ) {
-        super( context );
-        this.id = id;
-        this.outputPortId = outputPortId;
-        this.outputExpression = outputExpression;
-    }
+	public SolicitResponseExpressionNode(
+		ParsingContext context,
+		String id,
+		String outputPortId,
+		OLSyntaxNode outputExpression ) {
+		super( context );
+		this.id = id;
+		this.outputPortId = outputPortId;
+		this.outputExpression = outputExpression;
+	}
 
-    public String id() {
-        return id;
-    }
+	public String id() {
+		return id;
+	}
 
-    public String outputPortId() {
-        return outputPortId;
-    }
+	public String outputPortId() {
+		return outputPortId;
+	}
 
-    public OLSyntaxNode outputExpression() {
-        return outputExpression;
-    }
+	public OLSyntaxNode outputExpression() {
+		return outputExpression;
+	}
 
-    @Override
-    public < C, R > R accept( OLVisitor< C, R > visitor, C ctx ) {
-        return visitor.visit( this, ctx );
-    }
+	@Override
+	public < C, R > R accept( OLVisitor< C, R > visitor, C ctx ) {
+		return visitor.visit( this, ctx );
+	}
 
 }
