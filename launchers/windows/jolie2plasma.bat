42d258947bf7936e3010282f21147e59f0132310
Narongrit Unwerawattana <narongrit.kie@gmail.com>
2022-06-21 16:52:25 +0200

    update package path for windows launchers

--- launchers/windows/jolie2plasma.bat
+++ launchers/windows/jolie2plasma.bat
@@ -1,2 +1,2 @@
 @echo off
-java -cp %JOLIE_HOME%\jolie.jar;%JOLIE_HOME%\lib\libjolie.jar:%JOLIE_HOME%\tools\jolie2plasma.jar joliex.plasma.Jolie2Plasma -l %JOLIE_HOME%\lib;%JOLIE_HOME%\javaServices\*;%JOLIE_HOME%\extensions\* -i %JOLIE_HOME%\include -p %JOLIE_HOME%\packages;.\packages %*
+java -cp %JOLIE_HOME%\jolie.jar;%JOLIE_HOME%\lib\libjolie.jar:%JOLIE_HOME%\tools\jolie2plasma.jar joliex.plasma.Jolie2Plasma -l %JOLIE_HOME%\lib;%JOLIE_HOME%\javaServices\*;%JOLIE_HOME%\extensions\* -i %JOLIE_HOME%\include -p %JOLIE_HOME%\packages %*
