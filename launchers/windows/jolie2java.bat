49eaf977b71650357ebbf914a096d489bdd667b2
Narongrit Unwerawattana <narongrit.kie@gmail.com>
2022-06-21 16:52:25 +0200

    update package path for windows launchers

--- launchers/windows/jolie2java.bat
+++ launchers/windows/jolie2java.bat
@@ -1,2 +1,2 @@
 @echo off
-java -ea:jolie... -ea:joliex... -cp %JOLIE_HOME%\jolie-cli.jar;%JOLIE_HOME%\jolie.jar;%JOLIE_HOME%\lib\libjolie.jar;%JOLIE_HOME%\tools\jolie2java.jar;%JOLIE_HOME%\lib\jolie-gwt.jar joliex.java.Jolie2Java -l %JOLIE_HOME%\lib;%JOLIE_HOME%\javaServices\*;%JOLIE_HOME%\extensions\* -i %JOLIE_HOME%\include -p %JOLIE_HOME%\packages;.\packages %*
+java -ea:jolie... -ea:joliex... -cp %JOLIE_HOME%\jolie-cli.jar;%JOLIE_HOME%\jolie.jar;%JOLIE_HOME%\lib\libjolie.jar;%JOLIE_HOME%\tools\jolie2java.jar;%JOLIE_HOME%\lib\jolie-gwt.jar joliex.java.Jolie2Java -l %JOLIE_HOME%\lib;%JOLIE_HOME%\javaServices\*;%JOLIE_HOME%\extensions\* -i %JOLIE_HOME%\include -p %JOLIE_HOME%\packages %*
