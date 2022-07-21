988546ff2b7b61bae10c04644a741d1c6196e1ca
Fabrizio Montesi <famontesi@gmail.com>
2022-07-21 17:45:27 +0200

    Give some messages in dev-setup

--- scripts/dev-setup.sh
+++ scripts/dev-setup.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 BIN_DIR=`realpath $1`
+echo "Linking launchers in $BIN_DIR"
 ln -sf $PWD/dist/launchers/unix/* $BIN_DIR/
 
 DIST_DIR=$1/jolie-dist
-
+echo "Linking the Jolie distribution to $DIST_DIR"
 ln -sf $PWD/dist/jolie $DIST_DIR
+
+echo "Success! Make sure that your environment variable JOLIE_HOME is set to $DIST_DIR."
