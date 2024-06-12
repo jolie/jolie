c848798cce140c2ca0dc7bd73cea20515a31a292
Fabrizio Montesi <famontesi@gmail.com>
2024-06-12 13:58:09 +0200

    release script

---
+++ scripts/release.sh
@@ -0,0 +1,16 @@
+VER=$1
+if [ -z "$VER" ]; then
+	echo "Usage: $0 <version>"
+	exit 1
+fi
+
+echo "This will update pom.xml, create a new commit, and force push the tag v$VER"
+read -p "Are you sure that you want to proceed? (y/n) " answer
+if [[ $answer != "y" ]]; then
+	exit 0
+fi
+
+sed -i "s|<jolie.version>.*</jolie.version>|<jolie.version>$VER</jolie.version>|" pom.xml
+git commit -am "v$VER"
+git tag -f v$VER
+git push origin v$VER
